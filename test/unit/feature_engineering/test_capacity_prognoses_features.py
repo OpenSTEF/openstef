325db311bdd550196dd93cf9c8c0415cfca84171
Bram Harmsen <bram.harmsen@alliander.com>
2021-02-12 14:42:21 +0100

    Fix F821 errors (undefined name 'unittest')

--- test/unit/feature_engineering/test_capacity_prognoses_features.py
+++ test/unit/feature_engineering/test_capacity_prognoses_features.py
@@ -3,12 +3,13 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from test.utils.data import TestData
+import unittest
 
 import stf.feature_engineering.capacity_prognoses_features as cf
 
-data = TestData.load("input_data_train.pickle")
-
 from test.utils import TestData, BaseTestCase
+
+data = TestData.load("input_data_train.pickle")
 
 
 class TestCapacityPrognosesFeatures(BaseTestCase):
