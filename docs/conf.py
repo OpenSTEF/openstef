b1f2387f8ed740438087a798cc3e6d2eac4c7e45
Frank Kreuwel <fpmkreuwel@gmail.com>
2021-02-17 16:58:28 +0100

    sphinx_rtd_theme in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@
 import os
 import sys
 
-#import sphinx_rtd_theme
+import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../../ktprognoses/"))
 sys.path.insert(0, os.path.abspath("../../"))
@@ -58,8 +58,8 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-#html_theme = "sphinx_rtd_theme"
-#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
