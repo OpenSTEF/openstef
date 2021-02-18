b6ba1b0f091598c6273e255fe3161a8bdb5e6cbd
Frank Kreuwel <frank.kreuwel@alliander.com>
2021-02-18 16:23:11 +0100

    add ktpbase to path

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import sys
 import sphinx_rtd_theme
 
 sys.path.insert(0, os.path.abspath("../"))
+# for ktpbase
+sys.path.insert(0, os.path.abspath("../docs"))
 
 # -- Project information -----------------------------------------------------
 
