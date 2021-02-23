8d816b67241b1a5a8043ceb6bdec957cccb14592
black <action@github.com>
2021-02-23 16:08:59 +0000

    Format Python code with Black

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
-#    "recommonmark",
+    #    "recommonmark",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
