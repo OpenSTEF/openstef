2e3aa2ade3a76ce42250c41564d3a06b504d1267
gfsdekoning <gfsdekoning@gmail.com>
2021-02-26 15:10:32 +0100

    add test functions

--- test/unit/model/test_split_energy.py
+++ test/unit/model/test_split_energy.py
@@ -3,6 +3,9 @@
 # SPDX-License-Identifier: MPL-2.0
 
 import unittest
+from numpy import split
+
+from numpy.core.fromnumeric import mean
 from test.utils import BaseTestCase, TestData
 
 from openstf.model import split_energy
@@ -16,6 +19,36 @@ class TestSplitEnergy(BaseTestCase):
     def test_find_components(self):
         testcomponents, coefdict = split_energy.find_components(input_data)
         self.assertDataframeEqual(components, testcomponents, rtol=1e-3)
+
+    threshold = split_energy.COEF_MAX_PCT_DIFF
+
+    def test_are_coefs_valid_true(self):
+        new_coefs = {"a": 1, "b": 1, "c": -1}
+        mean_coefs = {"a": 1, "b": 1 + self.threshold, "c": -1}
+
+        result = split_energy.are_new_coefs_valid(new_coefs, mean_coefs)
+        self.assertTrue(result)
+
+    def test_are_coefs_valid_flipped_sign(self):
+        new_coefs = {"a": 1, "b": 1}
+        mean_coefs = {"a": 1, "b": -1}
+
+        result = split_energy.are_new_coefs_valid(new_coefs, mean_coefs)
+        self.assertFalse(result)
+
+    def test_are_coefs_valid_above_threshold(self):
+        new_coefs = {"a": 1, "b": 1}
+        mean_coefs = {"a": 1, "b": 1 + 1.5 * self.threshold}
+
+        result = split_energy.are_new_coefs_valid(new_coefs, mean_coefs)
+        self.assertFalse(result)
+
+    def test_are_coefs_valid_below_threshold(self):
+        new_coefs = {"a": 1, "b": 1}
+        mean_coefs = {"a": 1, "b": 1 - self.threshold}
+
+        result = split_energy.are_new_coefs_valid(new_coefs, mean_coefs)
+        self.assertFalse(result)
 
 
 # Run all tests
--- test/unit/monitoring/test_teams.py
+++ test/unit/monitoring/test_teams.py
@@ -34,6 +34,15 @@ class TestTeams(BaseTestCase):
         card_mock = teamsmock.connectorcard.return_value
         self.assertTrue(card_mock.send.called)
 
+    def test_build_sql_query_string(self, teamsmock):
+        query_df = pd.DataFrame(data=[["a", 1], ["b", 2]], columns=["key", "value"])
+        table = "table"
+
+        query_expected = "INSERT INTO table (key, value) VALUES \n('a', 1),\n('b', 2)"
+
+        query_result = teams.build_sql_query_string(query_df, table)
+        self.assertEqual(query_result, query_expected)
+
     def test_post_teams_better(self, teamsmock):
 
         test_feature_weights = pd.DataFrame(data={"gain": [1, 2]})
