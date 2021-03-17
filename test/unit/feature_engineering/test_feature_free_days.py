1a4bc278d63e46e83e797674c59b9e2748a33677
black <action@github.com>
2021-03-17 08:56:42 +0000

    Format Python code with Black

--- test/unit/feature_engineering/test_feature_free_days.py
+++ test/unit/feature_engineering/test_feature_free_days.py
@@ -7,18 +7,39 @@ from openstf.feature_engineering.feature_free_days import create_holiday_functio
 
 from test.utils import BaseTestCase
 
-expected_keys = ['is_national_holiday', 'is_nieuwjaarsdag', 'is_goede_vrijdag',
-                 'is_eerste_paasdag', 'is_tweede_paasdag', 'is_koningsdag',
-                 'is_bevrijdingsdag', 'is_bridgedaybevrijdingsdag', 'is_hemelvaart',
-                 'is_bridgedayhemelvaart', 'is_eerste_pinksterdag',
-                 'is_tweede_pinksterdag', 'is_eerste_kerstdag', 'is_tweede_kerstdag',
-                 'is_bridgedaykoningsdag', 'is_bridgeday', 'is_schoolholiday',
-                 'is_voorjaarsvakantiezuid', 'is_bouwvakmidden', 'is_bouwvakzuid',
-                 'is_meivakantie', 'is_zomervakantienoord', 'is_herfstvakantienoord',
-                 'is_kerstvakantie', 'is_voorjaarsvakantienoord',
-                 'is_voorjaarsvakantiemidden', 'is_herfstvakantiemidden',
-                 'is_bouwvaknoord', 'is_herfstvakantiezuid', 'is_zomervakantiezuid',
-                 'is_zomervakantiemidden']
+expected_keys = [
+    "is_national_holiday",
+    "is_nieuwjaarsdag",
+    "is_goede_vrijdag",
+    "is_eerste_paasdag",
+    "is_tweede_paasdag",
+    "is_koningsdag",
+    "is_bevrijdingsdag",
+    "is_bridgedaybevrijdingsdag",
+    "is_hemelvaart",
+    "is_bridgedayhemelvaart",
+    "is_eerste_pinksterdag",
+    "is_tweede_pinksterdag",
+    "is_eerste_kerstdag",
+    "is_tweede_kerstdag",
+    "is_bridgedaykoningsdag",
+    "is_bridgeday",
+    "is_schoolholiday",
+    "is_voorjaarsvakantiezuid",
+    "is_bouwvakmidden",
+    "is_bouwvakzuid",
+    "is_meivakantie",
+    "is_zomervakantienoord",
+    "is_herfstvakantienoord",
+    "is_kerstvakantie",
+    "is_voorjaarsvakantienoord",
+    "is_voorjaarsvakantiemidden",
+    "is_herfstvakantiemidden",
+    "is_bouwvaknoord",
+    "is_herfstvakantiezuid",
+    "is_zomervakantiezuid",
+    "is_zomervakantiemidden",
+]
 
 
 class GeneralTest(BaseTestCase):
