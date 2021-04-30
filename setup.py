af2e7752443321ccd913e74b722aa7e6fcd75903
Bram Harmsen <bram.harmsen@alliander.com>
2021-04-30 10:31:08 +0200

    Test to make beta release before final

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstf",
-    version="1.6.0",
+    version="1.6.1-beta.1",
     packages=find_packages(include=["openstf", "openstf.*"]),
     description="Open short term forcasting",
     long_description=read_long_description_from_readme(),
