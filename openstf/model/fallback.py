58ed053a672613295da05e2a860927ca06968c81
Bram Harmsen <bramharmsen@gmail.com>
2021-05-25 09:30:13 +0200

    Merged remote

--- openstf/model/fallback.py
+++ openstf/model/fallback.py
@@ -26,6 +26,10 @@ def generate_fallback(
     Returns:
         pandas.DataFrame: Fallback forecast DataFrame with columns:
             'forecast', 'quality'
+
+    Raises:
+        ValueError if len(load) == 0
+        NotImplementedError if fallback_strategy != 'extreme_day'
     """
     # Check if load is completely empty
     if len(load.dropna()) == 0:
@@ -36,30 +40,38 @@ def generate_fallback(
             f'fallback_strategy should be "extreme_day", received:{fallback_strategy}'
         )
 
-    # Find most extreme historic day (do not count today as it is incomplete)
-    day_with_highest_load_date = (
-        load[load.index.tz_localize(None).date != datetime.utcnow().date()]
-        .idxmax()
-        .load.date()
-    )
-    # generate datetime range of the day with the highest load
-    from_datetime = pd.Timestamp(day_with_highest_load_date, tz=load.index.tz)
-    till_datetime = from_datetime + pd.Timedelta("1 days")
+    if fallback_strategy == "extreme_day":
+        # Execute this fallback strategy
+        # Find most extreme historic day and merge it by time-of-day to the requested moments
 
-    # slice load dataframe, all rows for the day with the highest load
-    highest_daily_loadprofile = load.loc[
-        (load.index >= from_datetime) & (load.index < till_datetime)
-    ]
+        # Find most extreme historic day (do not count today as it is incomplete)
+        day_with_highest_load_date = (
+            load[load.index.tz_localize(None).date != datetime.utcnow().date()]
+            .idxmax()
+            .load.date()
+        )
+        # generate datetime range of the day with the highest load
+        from_datetime = pd.Timestamp(day_with_highest_load_date, tz=load.index.tz)
+        till_datetime = from_datetime + pd.Timedelta("1 days")
 
-    highest_daily_loadprofile.loc[:, "time"] = highest_daily_loadprofile.index.time
+        # slice load dataframe, only rows for the day with the highest load
+        highest_daily_loadprofile = load.loc[
+            (load.index >= from_datetime) & (load.index < till_datetime)
+        ]
 
-    forecast = pd.DataFrame(index=forecast_input)
-    forecast["time"] = forecast.index.time
-    forecast = (
-        forecast.reset_index()
-        .merge(highest_daily_loadprofile, left_on="time", right_on="time", how="outer")
-        .set_index("index")
-    )
+        # Match moments by time-of-day
+        highest_daily_loadprofile.loc[:, "time"] = highest_daily_loadprofile.index.time
+        forecast = pd.DataFrame(index=forecast_input)
+        forecast["time"] = forecast.index.time
+        forecast = (
+            forecast.reset_index()
+            .merge(
+                highest_daily_loadprofile, left_on="time", right_on="time", how="outer"
+            )
+            .set_index("index")
+        )
+
+    # Rename so column is called forecast
     forecast = forecast[["load"]].rename(columns=dict(load="forecast"))
 
     # Add a column quality.
