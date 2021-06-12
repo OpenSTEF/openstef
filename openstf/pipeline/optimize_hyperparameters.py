88f73a60735c5ceb9caac883f44b968fbb509120
Bram Harmsen <bramharmsen@gmail.com>
2021-06-12 13:13:24 +0200

    Add new files

---
+++ openstf/pipeline/optimize_hyperparameters.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+
+
+def optimize_hyperparameters_pipeline(pj: dict):
+
--- openstf/tasks/optimize_hyperparameters.py
+++ openstf/tasks/optimize_hyperparameters.py
@@ -13,12 +13,46 @@ Example:
     the CRON job can be found in the /k8s/CronJobs folder.
     Alternatively this code can be run directly by running::
 
-        $ python optimize_hyper_params.py
+        $ python optimize_hyperparameters.py
 
 """
+from datetime import datetime
+
+from ktpbase.database import DataBase
+
 from openstf.pipeline.optimize_hyperparameters import optimize_hyperparameters_pipeline
 from openstf.tasks.utils.predictionjobloop import PredictionJobLoop
 from openstf.tasks.utils.taskcontext import TaskContext
+from openstf.monitoring import teams
+
+MAX_AGE_HYPER_PARAMS_DAYS = 31
+
+
+def optimize_hyperparameters_task(pj: dict, context: TaskContext) -> None:
+
+    db = DataBase.get_instance()
+
+    if _last_optimimization_too_long_ago(pj) is not True:
+        context.logger.info("Hyperparameters not old enough to optimize again")
+        return
+
+    hyperparameters = optimize_hyperparameters_pipeline(pj)
+
+    db.write_hyper_params(pj, hyperparameters)
+
+    # Sent message to Teams
+    title = f'Optimized hyperparameters for prediction job {pj["name"]} {pj["description"]}'
+
+    teams.post_teams(teams.format_message(title=title, params=hyperparameters))
+
+def _last_optimimization_too_long_ago(pj):
+    db = DataBase.get_instance()
+    # Get data of last stored hyper parameters
+    previous_optimization_datetime = db.get_hyper_params_last_optimized(pj)
+
+    days_ago = (datetime.utcnow() - previous_optimization_datetime).days
+
+    return days_ago > MAX_AGE_HYPER_PARAMS_DAYS
 
 
 def main():
@@ -26,7 +60,7 @@ def main():
         model_type = ["xgb", "xgb_quantile", "lgb"]
 
         PredictionJobLoop(context, model_type=model_type).map(
-            optimize_hyperparameters_pipeline
+            optimize_hyperparameters_task, context
         )
 
 
