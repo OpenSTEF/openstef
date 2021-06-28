5ccafea50af026d7f02ae52b7d63e4e0c7ce94aa
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2021-06-28 23:01:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstf",
-    version="1.7.0a11",
+    version="1.7.0a12",
     packages=find_packages(include=["openstf", "openstf.*"]),
     description="Open short term forcasting",
     long_description=read_long_description_from_readme(),
