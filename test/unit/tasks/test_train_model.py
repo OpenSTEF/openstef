50de4bb45adad5ba9312aafd33d458ae302adc70
Frank Kreuwel <frank.kreuwel@alliander.com>
2021-07-14 13:37:31 +0200

    add unittests related to model age in train_model task

--- test/unit/tasks/test_train_model.py
+++ test/unit/tasks/test_train_model.py
@@ -5,6 +5,7 @@ from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
 from openstf.tasks.train_model import train_model_task
+from openstf.pipeline.train_model import MAXIMUM_MODEL_AGE
 
 from test.utils import TestData
 
@@ -25,3 +26,25 @@ class TestTrainModelTask(TestCase):
         self.assertEqual(
             train_model_pipeline_mock.call_args_list[0][0][0]["id"], self.pj["id"]
         )
+
+    @patch("openstf.tasks.train_model.get_model_age", return_value=MAXIMUM_MODEL_AGE-1)
+    @patch("openstf.tasks.train_model.train_model_pipeline")
+    def test_train_model_task_recent_old_model(self, train_model_pipeline_mock, get_model_age_mock):
+        """If an old model exists which is recent, abort the training immediately."""
+        # Test happy flow of create forecast task
+        context = MagicMock()
+        train_model_task(self.pj, context)
+        # Assert that the context is not called. This would be the case if input data was retrieved
+        context.database.get_model_input.assert_not_called()
+
+    @patch("openstf.tasks.train_model.get_model_age", return_value=MAXIMUM_MODEL_AGE+1)
+    @patch("openstf.tasks.train_model.train_model_pipeline")
+    def test_train_model_task_no_recent_old_model(self, train_model_pipeline_mock, get_model_age_mock):
+        """If an old model exists but is not recent, training should continue."""
+        # Test happy flow of create forecast task
+        context = MagicMock()
+        train_model_task(self.pj, context)
+        # Assert that the context is called. This would be the case if input data was retrieved
+        context.database.get_model_input.assert_called()
+
+
