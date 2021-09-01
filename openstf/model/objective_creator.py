854f3a104be9423cbe30ffe8f4eb604fd56a3455
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-09-01 10:49:48 +0200

    Bugfix in the object creator

--- openstf/model/objective_creator.py
+++ openstf/model/objective_creator.py
@@ -9,7 +9,7 @@ from openstf.model.objective import RegressorObjective, XGBRegressorObjective
 
 
 class ObjectiveCreator:
-    OBJECTIVES = {MLModelType.XGB: XGBRegressorObjective}
+    OBJECTIVES = {MLModelType.XGB.value: XGBRegressorObjective}
 
     @staticmethod
     def create_objective(model_type: Union[MLModelType, str]) -> RegressorObjective:
@@ -17,8 +17,8 @@ class ObjectiveCreator:
         if model_type not in valid_types:
             raise NotImplementedError(
                 f"No objective function for {model_type} valid model_types are:"
-                f"{', '.join([t.value for t in valid_types])}"
+                f"{', '.join([t for t in valid_types])}"
             )
-        model_type = MLModelType(model_type)
+        model_type = MLModelType(model_type).value
 
         return ObjectiveCreator.OBJECTIVES[model_type]
