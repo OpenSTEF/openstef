756ed0897c7b408e8450c5afe5beeaa31feadecd
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2021-09-13 14:21:48 +0200

    Update setup.py
    Bump version to 2.2.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstf",
-    version="2.2.2",
+    version="2.2.3",
     packages=find_packages(include=["openstf", "openstf.*"]),
     description="Open short term forcasting",
     long_description=read_long_description_from_readme(),
