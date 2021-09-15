01d986ec420dabf4f9efb0a87106429a0dff6449
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-09-15 16:24:15 +0200

    Logging now with pj_id

--- test/unit/validation/test_validation.py
+++ test/unit/validation/test_validation.py
@@ -15,6 +15,7 @@ class TestDataValidation(BaseTestCase):
         super().setUp()
         self.data_train = TestData.load("input_data_train.pickle")
         self.data_predict = TestData.load("input_data.pickle")
+        self.pj = TestData.get_prediction_job(pid=307)
 
     def test_clean(self):
 
@@ -25,8 +26,7 @@ class TestDataValidation(BaseTestCase):
     def test_validate(self):
 
         self.data_predict["load"][0:50] = 10.0
-        validated_data = validation.validate(self.data_predict)
-
+        validated_data = validation.validate(self.pj["id"], self.data_predict)
         self.assertEqual(len(validated_data[validated_data["load"].isna()]), 26)
 
     @patch("openstf.validation.validation.find_nonzero_flatliner")
@@ -45,7 +45,7 @@ class TestDataValidation(BaseTestCase):
         nonzero_flatliner_mock.return_value = suspicious_moments
         replace_invalid_data_mock.return_value = processed_input_data
 
-        validation.validate(data=processed_input_data)
+        validation.validate(self.pj["id"], data=processed_input_data)
 
         # simply check if all mocks are called
         for mock_func in [nonzero_flatliner_mock, replace_invalid_data_mock]:
