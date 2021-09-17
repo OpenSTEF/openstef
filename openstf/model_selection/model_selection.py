139bc4b60250bca221dd2e6a832833ee24d02d75
charlottecvn <charlottecvn@icloud.com>
2021-09-17 11:24:04 +0200

    Changed settings IDE to black
    Changed settings IDE to black

--- openstf/model_selection/model_selection.py
+++ openstf/model_selection/model_selection.py
@@ -212,10 +212,12 @@ def sample_indices_train_val(data, peaks, period_lengths, end_first, begin_last)
     for peak in peaks:
         if peak < (len(data)):
             start_point = int(peak * period_lengths) + end_first
-            if start_point < (len(data.index) - period_lengths + 1):
+            if start_point < (len(data) - period_lengths + 1):
                 end_point = int(start_point + period_lengths) - 1
-            else:
+            elif start_point < (len(data) - end_first + 1):
                 end_point = int(start_point + end_first) - 1
+            else:
+                end_point = int(start_point + 1)
             sampled |= set(range(start_point, end_point))
     return np.sort(list(sampled))
 
