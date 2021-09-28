5f60094daa5cd8f105a3f3aa955606e929f90464
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-09-28 13:06:07 +0200

    These test vanished from the unit-tests

--- test/unit/pipeline/test_optimize_hyperparameters.py
+++ test/unit/pipeline/test_optimize_hyperparameters.py
@@ -4,6 +4,8 @@
 import unittest
 from unittest.mock import patch
 
+import pandas as pd
+
 from openstf.exceptions import InputDataInsufficientError
 from test.utils import BaseTestCase, TestData
 from openstf.pipeline.optimize_hyperparameters import optimize_hyperparameters_pipeline
@@ -33,6 +35,27 @@ class TestOptimizeHyperParametersPipeline(BaseTestCase):
         with self.assertRaises(InputDataInsufficientError):
             optimize_hyperparameters_pipeline(pj, input_data)
 
+    def test_optimize_hyperparameters_pipeline_no_data(self, *args):
+        validation_mock = args[3]
+        validation_mock.is_data_sufficient.return_value = False
+
+        pj = TestData.get_prediction_job(pid=307)
+        input_data = pd.DataFrame()
+
+        # if there is no data a ValueError should be raised
+        with self.assertRaises(InputDataInsufficientError):
+            optimize_hyperparameters_pipeline(pj, input_data)
+
+    def test_optimize_hyperparameters_pipeline_no_load_data(self, *args):
+        validation_mock = args[3]
+        validation_mock.is_data_sufficient.return_value = False
+
+        pj = TestData.get_prediction_job(pid=307)
+        input_data = TestData.load("input_data_train.pickle")
+        input_data = input_data.drop("load", axis=1)
+        # if there is no data a ValueError should be raised
+        with self.assertRaises(InputDataInsufficientError):
+            optimize_hyperparameters_pipeline(pj, input_data)
 
 if __name__ == "__main__":
     unittest.main()
