a21ed54669aeb484231533d4029ca0b4f588e934
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2021-10-06 16:51:04 +0200

    added test

--- test/unit/feature_engineering/test_feature_applicator.py
+++ test/unit/feature_engineering/test_feature_applicator.py
@@ -20,12 +20,40 @@ class TestFeatureApplicator(TestCase):
         self.assertEqual(data_with_features.columns.to_list()[0], "load")
         self.assertEqual(data_with_features.columns.to_list()[-1], "horizon")
 
-    def test_operational_feature_applicator_correct_order(self):
+    def test_operational_feature_applicator_correct_order_horizon_not_in_output(self):
         # Test for expected column order of the output
         # Also check "horizons" is not in the output
         input_data = TestData.load("input_data.pickle")
+        requested_features = [
+            "IsSunday",
+            "IsWeekDay",
+            "IsWeekendDay",
+            "Month",
+            "Quarter",
+        ]
+
         data_with_features = OperationalPredictFeatureApplicator(
-            horizons=[0.25]
+            horizons=[0.25], feature_names=requested_features
         ).add_features(input_data[["load"]])
         self.assertEqual(data_with_features.columns.to_list()[0], "load")
         self.assertTrue("horizon" not in data_with_features.columns.to_list())
+
+    def test_operational_feature_applicator_correct_order_horizon_in_output(self):
+        # Test for expected column order of the output
+        # Also check "horizons" is not in the output
+        input_data = TestData.load("input_data.pickle")
+        requested_features = [
+            "IsSunday",
+            "IsWeekDay",
+            "IsWeekendDay",
+            "Month",
+            "Quarter",
+            "horizon",
+        ]
+
+        data_with_features = OperationalPredictFeatureApplicator(
+            horizons=[0.25], feature_names=requested_features
+        ).add_features(input_data[["load"]])
+        self.assertEqual(data_with_features.columns.to_list()[0], "load")
+        self.assertTrue("horizon" in data_with_features.columns.to_list())
+        self.assertFalse(data_with_features["horizon"].isna().all())
