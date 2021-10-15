697fa6fc8222b364574f8f94878f97dcd4d97e43
bhardier <benoit.hardier@probayes.com>
2021-10-15 14:13:59 +0200

    Add missing value handler.

---
+++ openstf/model/metamodels/missing_values_handler.py
@@ -0,0 +1,119 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, RegressorMixin, MetaEstimatorMixin, clone
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.utils.validation import check_is_fitted, check_X_y, check_array
+
+
+class MissingValueHandler(BaseEstimator, RegressorMixin, MetaEstimatorMixin):
+    """Class for a meta-models that handles missing values and removes columns filled exclusively by NaN.
+    Pipeline of imputer for missing value and a regressor.
+
+    Parameters
+    ----------
+
+    base_estimator: RegressorMixin
+        Regressor used in the pipeline.
+
+    missing_values: default=np.nan
+        Type of missing values handled by the imputer.
+
+    imputation_strategy: str, default=None
+        Imputing strategy for the imputer. If None, nan values are not allow and the linear regression will raise a ValueError.
+
+    fill_value: optional
+        Value used by the imputer for missing values.
+
+     Attributes
+    ----------
+        feature_names: list(str)
+            All input feature.
+
+        non_null_columns_: list(str)
+            Valid features used by the regressor.
+
+        n_features_in_: int
+            Number of input features.
+
+        regressor_: RegressorMixin
+            Regressor fitted on valid columns.
+
+        imputer_: SimpleImputer
+            Imputer for missig value fitted on valid columns.
+
+        pipeline_: Pipeline
+            Pipeline that chains the imputer and the regressor.
+
+        feature_importances_: ndarray (n_features_in_, )
+            The feature importances from the regressor for valid features and zero otherwise.
+    """
+
+    def __init__(
+        self,
+        base_estimator,
+        missing_values=np.nan,
+        imputation_strategy=None,
+        fill_value=None,
+    ):
+        self.base_estimator = base_estimator
+        self.missing_values = missing_values
+        self.imputation_strategy = imputation_strategy
+        self.fill_value = fill_value
+
+    def _get_tags(self):
+        tags = self.base_estimator._get_tags()
+        tags["allow_nan"] = self.imputation_strategy is not None
+        return tags
+
+    def fit(self, X, y, **kwargs):
+
+        _, y = check_X_y(X, y, force_all_finite="allow-nan", y_numeric=True)
+        if type(X) != pd.DataFrame:
+            X = pd.DataFrame(np.asarray(X))
+
+        # Remove always null columns
+        columns = X.isnull().all(0)
+        self.feature_names_ = list(X.columns)
+        self.non_null_columns_ = list(columns[~columns].index)
+        self.n_features_in_ = X.shape[1]
+
+        self.regressor_ = clone(self.base_estimator)
+        if self.imputation_strategy is None:
+            self.imputer_ = None
+            self.pipeline_ = Pipeline([("regressor", self.regressor_)])
+        else:
+            self.imputer_ = SimpleImputer(
+                missing_values=self.missing_values,
+                strategy=self.imputation_strategy,
+                fill_value=self.fill_value,
+            )
+            self.pipeline_ = Pipeline(
+                [("imputer", self.imputer_), ("regressor", self.regressor_)]
+            )
+
+        self.pipeline_.fit(X[self.non_null_columns_], y)
+
+        if hasattr(self.regressor_, "feature_importances_"):
+            reg_feature_importances = self.regressor_.feature_importances_
+            self.feature_importances_ = np.zeros(self.n_features_in_)
+            j = 0
+            for i, c in enumerate(self.feature_names_):
+                if c in self.non_null_columns_:
+                    self.feature_importances_[i] = reg_feature_importances[j]
+                    j += 1
+
+        return self
+
+    def predict(self, X):
+        check_is_fitted(self)
+        check_array(
+            X,
+            force_all_finite="allow-nan",
+        )
+        if type(X) != pd.DataFrame:
+            X = pd.DataFrame(np.array(X))
+        return self.pipeline_.predict(X[self.non_null_columns_])
--- openstf/model/regressors/linear.py
+++ openstf/model/regressors/linear.py
@@ -1,115 +1,52 @@
 # SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-import pandas as pd
+from openstf.model.metamodels.missing_values_handler import MissingValueHandler
 import numpy as np
-from sklearn.base import RegressorMixin, BaseEstimator
 from sklearn.linear_model import LinearRegression
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.validation import check_is_fitted
 
 from openstf.model.regressors.regressor import OpenstfRegressor
 
 
-class LinearRegressor(BaseEstimator, RegressorMixin):
-    """Class for Linear Models that handles missing values,
+class LinearRegressor(LinearRegression):
+    """Wrapper around sklearn linearRegression,
+    that provides features importances as absolute values of the coefficients."""
 
-    Pipeline of imputer for missing value and linear regressor.
+    def __init__(self):
+        super().__init__()
 
-    Parameters
-    ----------
-    missing_values: default=np.nan
-        Type of missing values handled by the imputer.
+    def fit(self, X, y):
+        super().fit(X, y)
+        self.feature_importances_ = np.abs(self.coef_)
 
-    imputation_strategy: str, default=None
-        Imputing strategy for the imputer. If None, nan values are not allow and the linear regression will raise a ValueError.
 
-    fill_value: optional
-        Value used by the imputer for missing values.
+class LinearRTEOpenstfRegressor(OpenstfRegressor):
+    gain_importance_name = "total_gain"
+    weight_importance_name = "weight"
 
-     Attributes
-    ----------
-        feature_names: list(str)
-            All input feature.
-
-        non_null_columns_: list(str)
-            Valid features used by the regressor.
-
-        n_features_in_: int
-            Number of input features.
-
-        regressor_: LinearRegression
-            Linear regressor on valid columns.
-
-        imputer_: SimpleImputer
-            Imputer for missig value.
-
-        pipeline_: Pipeline
-            Pipeline that chains imputer and linear regressor.
-
-        feature_importances_: ndarray (n_features_in_, )
-            The absolute values of the regressions' coefficients for valid feratures and zero otherwise.
-    """
-
-    def __init__(
-        self, missing_values=np.nan, imputation_strategy=None, fill_value=None
-    ):
-        self.missing_values = missing_values
+    def __init__(self, imputation_strategy="mean"):
+        super().__init__()
         self.imputation_strategy = imputation_strategy
-        self.fill_value = fill_value
+
+    def _more_tags(self):
+        return {"allow_nan": self.imputation_strategy is not None}
 
     def fit(self, X, y, **kwargs):
-        _, y = check_X_y(X, y, force_all_finite="allow-nan", y_numeric=True)
-        if type(X) != pd.DataFrame:
-            X = pd.DataFrame(np.asarray(X))
+        self.estimator_ = MissingValueHandler(
+            LinearRegressor(), imputation_strategy=self.imputation_strategy
+        )
 
-        # Remove always null columns
-        columns = X.isnull().all(0)
-        self.feature_names_ = list(X.columns)
-        self.non_null_columns_ = list(columns[~columns].index)
-        self.n_features_in_ = X.shape[1]
-
-        self.regressor_ = LinearRegression()
-        if self.imputation_strategy is None:
-            self.imputer_ = None
-            self.pipeline_ = Pipeline([("regressor", self.regressor_)])
-        else:
-            self.imputer_ = SimpleImputer(
-                missing_values=self.missing_values,
-                strategy=self.imputation_strategy,
-                fill_value=self.fill_value,
-            )
-            self.pipeline_ = Pipeline(
-                [("imputer", self.imputer_), ("regressor", self.regressor_)]
-            )
-
-        self.pipeline_.fit(X[self.non_null_columns_], y)
-
-        reg_feature_importances = np.abs(self.regressor_.coef_)
-        self.feature_importances_ = np.zeros(len(self.feature_names))
-        j = 0
-        for i, c in enumerate(self.feature_names):
-            if c in self.non_null_columns_:
-                self.feature_importances_[i] = reg_feature_importances[j]
-                j += 1
+        self.estimator_.fit(X, y)
+        self.feature_names_ = self.estimator_.feature_names_
+        self.n_features_in_ = self.estimator_.n_features_in_
+        self.feature_importances_ = self.estimator_.feature_importances_
 
         return self
 
     def predict(self, X):
         check_is_fitted(self)
-        check_array(
-            X,
-            force_all_finite="allow-nan",
-        )
-        if type(X) != pd.DataFrame:
-            X = pd.DataFrame(np.array(X))
-        return self.pipeline_.predict(X[self.non_null_columns_])
-
-
-class LinearRTEOpenstfRegressor(LinearRegressor, OpenstfRegressor):
-    gain_importance_name = "total_gain"
-    weight_importance_name = "weight"
+        return self.estimator_.predict(X)
 
     @property
     def feature_names(self):
