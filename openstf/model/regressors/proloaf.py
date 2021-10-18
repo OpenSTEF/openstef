f8ef7b526e3e7333e950b04d63bbfaea949480ed
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-10-18 15:29:25 +0200

    Specify parameters for proloaf

--- openstf/model/regressors/proloaf.py
+++ openstf/model/regressors/proloaf.py
@@ -1,6 +1,3 @@
-import json
-from pathlib import Path
-
 import numpy as np
 import pandas as pd
 import torch
@@ -11,12 +8,56 @@ from utils.modelhandler import ModelWrapper
 
 class OpenstfProloafRegressor(OpenstfRegressor, ModelWrapper):
     def __init__(
-        self, json_path: Path = "model/regressors/parameters/proloaf_parameters.json"
-    ):
-        with open(json_path) as json_file:
-            parameters = json.load(json_file)
+        self,
+        name: str = "model",
+        core_net: str = "torch.nn.LSTM",
+        relu_leak: float = 0.1,
+        encoder_features: List[str] = None,
+        decoder_features: List[str] = None,
+        core_layers: int = 1,
+        rel_linear_hidden_size: float = 1.0,
+        rel_core_hidden_size: float = 1.0,
+        dropout_fc: float = 0.4,
+        dropout_core: float = 0.3,
+        training_metric: str = "nllgaus",
+        metric_options: Dict[str, Any] = {},
+        optimizer_name: str = "adam",
+        early_stopping_patience: int = 7,
+        early_stopping_margin: float = 0.0,
+        learning_rate: float = 1e-4,
+        max_epochs: int = 100,
+        device: str = "cpu",
+        batch_size: int = 10,
+        split_percent: float = 0.85,
+        history_horizon: int = 24,
+        forecast_horizon: int = None,
 
-        ModelWrapper.__init__(self, **parameters)
+    ):
+        self.device = device
+        self.batch_size = batch_size
+        self.split_percent = split_percent
+        self.history_horizon = history_horizon
+        self.forecast_horizon = forecast_horizon
+        ModelWrapper.__init__(
+            self,
+            name=name,
+            core_net=core_net,
+            relu_leak=relu_leak,
+            encoder_features=encoder_features,
+            decoder_features=decoder_features,
+            core_layers=core_layers,
+            rel_linear_hidden_size=rel_linear_hidden_size,
+            rel_core_hidden_size=rel_core_hidden_size,
+            dropout_fc=dropout_fc,
+            dropout_core=dropout_core,
+            training_metric=training_metric,
+            metric_options=metric_options,
+            optimizer_name=optimizer_name,
+            early_stopping_patience=early_stopping_patience,
+            early_stopping_margin=early_stopping_margin,
+            learning_rate=learning_rate,
+            max_epochs=max_epochs,
+        )
 
     def predict(self, x: pd.DataFrame) -> np.ndarray:
         # x = dh.fill_if_missing(x, periodicity=24)
@@ -30,11 +71,6 @@ class OpenstfProloafRegressor(OpenstfRegressor, ModelWrapper):
         self,
         x: pd.DataFrame,
         y: pd.DataFrame,
-        device: str = "cpu",
-        batch_size: int = 10,
-        split_percent: float = 0.85,
-        history_horizon: int = 24,
-        forecast_horizon: int = 24,
         eval_set: tuple = None,
         early_stopping_rounds: int = None,
         verbose: bool = False,
@@ -46,13 +82,13 @@ class OpenstfProloafRegressor(OpenstfRegressor, ModelWrapper):
             df=df,  # TODO this needs to be x and y cacatinated
             encoder_features=self.encoder_features,
             decoder_features=self.decoder_features,
-            batch_size=batch_size,
-            history_horizon=history_horizon,
-            forecast_horizon=forecast_horizon,
+            batch_size=self.batch_size,
+            history_horizon=self.history_horizon,
+            forecast_horizon=self.forecast_horizon,
             target_id=self.target_id,
-            train_split=split_percent,
+            train_split=self.split_percent,
             validation_split=1.0,
-            device=device,
+            device=self.device,
         )
         self.to(device)
         self.init_model()
@@ -63,18 +99,9 @@ class OpenstfProloafRegressor(OpenstfRegressor, ModelWrapper):
         training_params = self.get_training_config()
         return {**model_params, **training_params}
 
-    def set_params(
-        self,
-        encoder_features=["feature_a", "feature_b"],
-        decoder_features=["feature_a", "feature_b"],
-        **params
-    ):
+    def set_params(self,**params):
         self.update(**params)
-        self.update(encoder_features=encoder_features)
-        self.update(decoder_features=decoder_features)
-
         return self
-
 
 if __name__ == "__main__":
     test = OpenstfProloafRegressor()
--- openstf/pipeline/train_model.py
+++ openstf/pipeline/train_model.py
@@ -242,7 +242,7 @@ def train_pipeline_common(
         )
 
     # Create relevant model
-    model = ModelCreator.create_model(pj["model"], quantiles=pj["quantiles"])
+    model = ModelCreator.create_model(pj["model"], quantiles=pj["quantiles"], forecast_horizon=pj["horizon_minutes"])
 
     # split x and y data
     train_x, train_y = train_data.iloc[:, 1:-1], train_data.iloc[:, 0]
