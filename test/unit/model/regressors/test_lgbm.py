644a921d654673d680967c34b65404df856a9569
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-10-18 11:27:26 +0200

    Add unit tests

---
+++ test/unit/model/regressors/test_lgbm.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+from test.utils import BaseTestCase
+from openstf.model.regressors.lgbm import LGBMOpenstfRegressor
+
+
+class TestXGB(BaseTestCase):
+    def setUp(self) -> None:
+        self.model = LGBMOpenstfRegressor()
+
+    def test_importance_names(self):
+        self.assertIsInstance(self.model._get_importance_names(), dict)
+
+
+
