3558fafc2cfd473677dc598b939a22f96f33b909
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2021-10-28 11:48:29 +0200

    Changed license from Alliander N.V. to Contributors to the OpenSTF project

--- openstf/model/objective.py
+++ openstf/model/objective.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+# SPDX-FileCopyrightText: 2017-2021 Contributors to the OpenSTF project <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
 from datetime import datetime
