929b8ca350479c2b8cf14a057588bb92d9904e1b
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-11-02 14:13:48 +0100

    change csv to new format

--- test/unit/feature_engineering/test_apply_features.py
+++ test/unit/feature_engineering/test_apply_features.py
@@ -100,10 +100,6 @@ class TestApplyFeaturesModule(BaseTestCase):
             TestData.load("input_data.pickle")
         )
 
-        test_data_load = TestData.load("input_data_multi_horizon_features.csv")
-
-        print(set(test_data_load.columns.unique())-set(input_data_with_features.columns.unique()))
-
         self.assertDataframeEqual(
             input_data_with_features,
             TestData.load("input_data_multi_horizon_features.csv"),
