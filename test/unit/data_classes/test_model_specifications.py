84202af9e190905c7b12bcc33fd9f6196ede5a66
Enrico Schmitz <26628063+EnricoASchmitz@users.noreply.github.com>
2021-11-10 13:52:51 +0100

    Unittests for model_specification.py

---
+++ test/unit/data_classes/test_model_specifications.py
@@ -0,0 +1,25 @@
+import unittest
+from test.utils.data import TestData
+
+class TestModelSpecifications(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.pj, self.modelspecs = TestData.get_prediction_job_and_modelspecs(307)
+
+    def test_model_specifications_get(self):
+        fn = self.modelspecs.__getitem__("feature_names")
+        self.assertIsInstance(fn, list)
+
+    def test_model_specifications_set(self):
+        hyper_params_test = {"set": "succes"}
+        self.modelspecs.__setitem__("hyper_params", hyper_params_test)
+        self.assertEqual(hyper_params_test, self.modelspecs.hyper_params)
+
+    def test_model_specifications_set_unknown_attribute(self):
+        with self.assertRaises(AttributeError):
+            self.modelspecs.__setitem__("unknown", "shouldn't set")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
