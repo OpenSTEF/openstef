8ba3cc7d13bbfebe1a399a4b531481cc7d27497f
bhardier <benoit.hardier@probayes.com>
2021-11-16 12:11:57 +0100

    Add FeatureAdder and Dispatcher

---
+++ openstf/feature_engineering/feature_adder.py
@@ -0,0 +1,128 @@
+# SPDX-FileCopyrightText: 2017-2021 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+from abc import ABC, abstractmethod
+from typing import List, Dict, Tuple, Sequence, Optional
+from collections import namedtuple, Counter
+import pandas as pd
+import re
+
+
+ParsedFeature = namedtuple("ParsedFeature", ["name", "params"])
+
+
+class FeatureAdder(ABC):
+    @property
+    @abstractmethod
+    def _regex(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @abstractmethod
+    def required_features(self, feature_names) -> List[str]:
+        ...
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def parse_feature_name(self, feature_name: str) -> Optional[Dict[str, str]]:
+        """Parse a feature name
+
+        If the feature name is taken in charge by the feature adder, the method returns
+        a dictionnary with the potentially parsed parameters contained the feature name. In the
+        case the feature name does not contain parameters an empty dictionary is returned.
+        Otherwise the method returns None.
+
+        Parameters
+        ----------
+        feature_name: str
+            The feature name. The feature name may contain parameter informations
+
+        Returns
+        -------
+        parameters: Optional[Dict[str, Any]]
+            The parsed parameters. If the feature name is recognized but has no parameters
+            an empty dictionnary is returned. If the feature name is not recognized, None is
+            returned.
+        """
+        reg = self._regex
+        match = re.match(reg, feature_name)
+        return None if match is None else match.groupdict()
+
+    @abstractmethod
+    def apply_features(
+        self, df: pd.DataFrame, parsed_feature_names: Sequence[ParsedFeature]
+    ) -> pd.DataFrame:
+        ...
+
+    def __repr__(self):
+        return "%s(<%s>)" % (self.__class__.__name__, self.name)
+
+
+class FeatureDispatcher:
+    def __init__(self, feature_adders: Sequence[FeatureAdder]):
+        self.feature_adders = list(feature_adders)
+        self._check_feature_adder_names_unicity()
+
+    def _check_feature_adder_names_unicity(self):
+        names = Counter(adder.name for adder in self.feature_adders)
+        duplicated_names = []
+        for name, count in names.items():
+            if count > 1:
+                duplicated_names.append(name)
+
+        if len(duplicated_names) > 0:
+            raise RuntimeError(
+                "There is at least one duplicated feature adder name: %s"
+                % duplicated_names
+            )
+
+    def dispatch_features(
+        self, feature_names
+    ) -> Dict[FeatureAdder, List[ParsedFeature]]:
+        recognized_features = set()
+        dispatched_features = {}
+
+        for feature_name in feature_names:
+            for adder_obj in self.feature_adders:
+                parsed_params = adder_obj.parse_feature_name(feature_name)
+                if parsed_params is not None:
+                    if feature_name in recognized_features:
+                        raise RuntimeError(
+                            "Ambiguous feature adder set detected. The feature name '%s'"
+                            " is recognised by more than 1 feature adder"
+                            % feature_names
+                        )
+                    recognized_features.add(feature_name)
+                    features = dispatched_features.setdefault(adder_obj, [])
+                    features.append(ParsedFeature(feature_name, parsed_params))
+
+        return dispatched_features
+
+    def apply_features(self, df, feature_names):
+        dispatched_features = self.dispatch_features(feature_names)
+
+        applied_features = set()
+        applied_features_num = 0
+
+        while True:
+            for adder, parsed_features in dispatched_features.items():
+                parsed_feature_names = [pf.name for pf in parsed_features]
+                required_features = adder.required_features(parsed_feature_names)
+
+                if len(set(required_features) - set(df.columns)) == 0:
+                    df = adder.apply_features(df, parsed_features)
+                    applied_features |= set(parsed_feature_names)
+
+            if (
+                len(applied_features) == applied_features_num
+            ):  # No new feature was treated
+                break
+
+            applied_features_num = len(applied_features)
+
+        return df
