8f9b6691f049b5d1d9c9450262336ef934a5e4b3
black <action@github.com>
2021-11-29 13:38:06 +0000

    Format Python code with Black

--- setup.py
+++ setup.py
@@ -49,7 +49,9 @@ setup(
     install_requires=read_requirements_from_file(),
     setup_requires=["wheel", "Cython"],
     tests_require=["pytest", "pytest-cov", "flake8"],
-    extras_require={"proloaf":["proloaf==0.2.0", "torch==1.10.0", "pytorch-lightning==1.5.1"]},
+    extras_require={
+        "proloaf": ["proloaf==0.2.0", "torch==1.10.0", "pytorch-lightning==1.5.1"]
+    },
     classifiers=[
         r"Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
