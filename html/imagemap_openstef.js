d25c1d3905f70d7bcc4ec7d2ef49d6eb339d7d80
Frank Kreuwel <frank.kreuwel@alliander.com>
2021-12-23 10:25:09 +0100

    minor suggestions for changes in text

--- html/imagemap_openstef.js
+++ html/imagemap_openstef.js
@@ -4,30 +4,30 @@
 var map = $('#OPENSTEF')
 var captions = {
         total_load: ["Total Load",
-                      "Total load for a specific substation which can be set above. "
-                      + "Last part of the time series in the prognose (part with green area "
+                      "Total load for a specific substation which can be selected above. "
+                      + "Last part of the time series in the forecast (part with green area "
                       + "under the curve)"],
 		forecast_realised: ["Forecast and Realised",
-                      "Realised load and forecasted load of different timestamps ahead. "
+                      "Realised load and forecasted load of different lead times. "
                       + "Last forecast is also shown. "
                       + "Values are given with an certain confidence band"],
 		predictor_split: ["Predictors",
-                      "Predictor for a specific horizon in action. "
+                      "Predictor for a specific lead time in action. "
                       + "Splitting of data (train, test and validation,  "
                       + "used to train the model with the specific horizon. "
-		      + "Splitted data is specified for the realised and the forecasted data. "],
+		      + "Splitted data is shown for the realised and the forecasted data. "],
 		information: ["Information",
                       "Information regarding the substation and forecast. "
-                      + "Information contain the last measurement, days available of prediction, "
+                      + "Information containing the last measurement, days available of prediction, "
 		      + "last update of the information, the state of the forecast quality ( "
-                      + "actual or not renewed), EAN for the substation (if available) and "
-		      + "the API for the substation (if available)"],
+                      + "actual, not renewed or substituted), EAN for the substation (if available) "
+		      + "and the API for the substation (if available)"],
 		parameters: ["Adjustable input for the dashboard",
-                      "Values that can be adjusted in order to select the wanted input: "
+                      "Values that can be selected in order to view the desired input: "
                       + "\n * Prediction: location of substation "
                       + "\n * PID: unique integer value related to the substation "
                       + "\n * Confidence: used confidence interval for forecasting "
-                      + "\n * SpecificTAhead: select the wanted time stamps ahead as start point"
+                      + "\n * SpecificTAhead: select forecast made at specified lead time"
                       + "\n * PID: unique integer value related to the substation "],
 		location:["Location related to the substation",
                       "Location is specified by latitude and longitude. "],
@@ -37,17 +37,17 @@ var captions = {
                       + "the more important the feature is for the forecasting of "
                       + "the specific substation, importance and weight of feature "
                       + "is given in percentage. "],
-		flatliners: ["Presence of flatliners",
-                      "If flatliners are present in the data, "
-                      + "they are highlighted with an red colored area for the given period."],
+		suspicious_input: ["Suspicious inputs",
+                      "Periods with suspicious input data is detected, "
+                      + "which are highlighted with a red colored area."],
 		energy_split_plot: ["Split components",
                       "Forecast is split into solar (pv) , wind and other energy. "
                       + "Plotted for those categories, including the main forecast"],
 		energy_split_tab: ["Energy splitting coefficients",
                       "In order to split forecast into solar, wind and other energy, "
                       + "coefficients must be specified. In the table the coefficients "
-                      + "are fiven for each component (wind solar and energy profiles). "
-                      + "The time when coefficients were calculate, is specified"],
+                      + "are given for each component (wind solar and energy profiles). "
+                      + "The time when coefficients were calculate, is also shown"],
             }
 var single_opts = {
                 fillColor: '000000',
