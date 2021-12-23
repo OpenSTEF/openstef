faddf2b77e25a1a1622e3e24d1e653bde210def7
Charlotte Cambier van Nooten <charlotte.cambier.van.nooten@alliander.com>
2021-12-23 09:44:14 +0100

    updated js for the interactive imagemap

--- html/imagemap_openstef.js
+++ html/imagemap_openstef.js
@@ -1,8 +1,5 @@
-<!--
-SPDX-FileCopyrightText: 2017-2021 Contributors to the OpenSTF project <korte.termijn.prognoses@alliander.com>
-
-SPDX-License-Identifier: MPL-2.0
--->
+// SPDX-FileCopyrightText: 2017-2021 Contributors to the OpenSTF project <korte.termijn.prognoses@alliander.com>
+// SPDX-License-Identifier: MPL-2.0
 
 var map = $('#OPENSTEF')
 var captions = {
