6a2934596390f2264a8fa8ab2eb5cc6c65272567
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2022-02-15 15:17:43 +0100

    Update prediction_job.py

--- openstef/data_classes/prediction_job.py
+++ openstef/data_classes/prediction_job.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from typing import Union, Optional, List
 
 from pydantic import BaseModel
