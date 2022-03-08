c1879e438cc4c9cb12408253f0ac2365350d8f56
Benoit Hardier <benoit.hardier@probayes.com>
2022-03-08 14:00:32 +0100

    migration of test for the grouped regressor
    Signed-off-by: Benoit Hardier <benoit.hardier@probayes.com>

---
+++ test/unit/model/metamodels/test_grouped_regressor.py
@@ -0,0 +1,96 @@
+# SPDX-FileCopyrightText: 2017-2022 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+import sklearn
+from lightgbm import LGBMRegressor
+
+from openstef.model.regressors.linear import LinearRegressor
+from openstef.model.metamodels.grouped_regressor import GroupedRegressor
+from test.unit.utils.base import BaseTestCase
+from test.unit.utils.data import TestData
+
+
+class TestGroupedRegressor(BaseTestCase):
+    def setUp(self):
+        self.train_input = TestData.load("reference_sets/307-train-data.csv")
+
+        self.train_with_time = self.train_input.copy(deep=True)
+        self.train_with_time["time"] = self.train_with_time.index.time
+
+        self.train_x = self.train_with_time.iloc[:, 1:]
+        self.train_y = self.train_with_time.iloc[:, 0]
+        self.val_x = self.train_input.iloc[:, 1:]
+        self.val_y = self.train_input.iloc[:, 0]
+
+    def test_missing_group_columns(self):
+        model = GroupedRegressor(LinearRegressor(), group_columns=["time"])
+        model_without_group = GroupedRegressor(LinearRegressor(), group_columns=None)
+
+        # test handling of group columns
+        with self.assertRaises(ValueError):
+            model_without_group.fit(self.train_x, self.train_y)
+
+        with self.assertRaises(ValueError):
+            model.fit(self.val_x, self.val_y)
+
+    def test_fit(self):
+        model = GroupedRegressor(LinearRegressor(), group_columns=["time"])
+        # test fitting metamodel
+        model.fit(self.train_x, self.train_y)
+        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model))
+
+    def test_parallel_fitting(self):
+        model_parallel = GroupedRegressor(
+            LinearRegressor(), group_columns="time", n_jobs=4
+        )
+        # test parallel fitting
+        model_parallel.fit(self.train_x, self.train_y)
+        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model_parallel))
+
+    def test_prediction(self):
+        model = GroupedRegressor(LinearRegressor(), group_columns=["time"])
+        model.fit(self.train_x, self.train_y)
+        # test prediction
+        res = model.predict(self.train_x)
+        group = self.train_with_time.iloc[:, -1]
+        for k, estimator in model.estimators_.items():
+            self.assertTrue(
+                (
+                    res[group == k]
+                    == estimator.predict(
+                        self.train_with_time.loc[group == k].iloc[:, 1:-1]
+                    )
+                ).all()
+            )
+
+        # print(self.train_x.reset_index(drop=True).sample(20).index)
+        # print(model.predict(self.train_x.reset_index(drop=True).sample(20)))
+        # self.assertFalse((model.predict(self.train_x.sample(20)) == np.nan).any())
+
+    def test_kwargs_handling(self):
+        # test kwargs in fit and predict methods
+        model_lgb = GroupedRegressor(LGBMRegressor(), group_columns="time")
+        group = self.train_with_time.iloc[:, -1]
+        with self.assertRaises(ValueError):
+            model_lgb.fit(
+                self.train_x, self.train_y, eval_set=[(self.val_x, self.val_y)]
+            )
+
+        model_lgb.fit(
+            self.train_x,
+            self.train_y,
+            eval_set=[(self.train_x, self.train_y)],
+            verbose=False,
+        )
+        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model_lgb))
+        res = model_lgb.predict(self.train_x, raw_score=True)
+        for k, estimator in model_lgb.estimators_.items():
+            self.assertTrue(
+                (
+                    res[group == k]
+                    == estimator.predict(
+                        self.train_with_time.loc[group == k].iloc[:, 1:-1],
+                        raw_score=True,
+                    )
+                ).all()
+            )
--- test/unit/model/regressors/test_linear.py
+++ test/unit/model/regressors/test_linear.py
@@ -1,17 +1,13 @@
 # SPDX-FileCopyrightText: 2017-2022 Alliander N.V. <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-from datetime import datetime
-import unittest
 
 import numpy as np
 import pandas as pd
 import sklearn
 from sklearn.utils.estimator_checks import check_estimator
-from lightgbm import LGBMRegressor
 
-from openstef.model.regressors.linear import LinearOpenstfRegressor, LinearRegressor
-from openstef.model.metamodels.grouped_regressor import GroupedRegressor
+from openstef.model.regressors.linear import LinearOpenstfRegressor
 from test.unit.utils.base import BaseTestCase
 from test.unit.utils.data import TestData
 
@@ -105,66 +101,3 @@ class TestLinearOpenstfRegressor(BaseTestCase):
         # check the retrieval of feature importance
         self.assertTrue((feature_importance_linear == feature_importance_model).all())
         self.assertTrue((feature_importance_null == 0).all())
-
-    def test_grouped_regressor(self):
-        model = GroupedRegressor(LinearRegressor(), group_columns=["time"])
-        model_parallel = GroupedRegressor(
-            LinearRegressor(), group_columns="time", n_jobs=4
-        )
-        model_without_group = GroupedRegressor(LinearRegressor(), group_columns=None)
-
-        train_with_time = self.train_input.copy(deep=True)
-        train_with_time["time"] = train_with_time.index.time
-
-        train_x = train_with_time.iloc[:, 1:]
-        train_y = train_with_time.iloc[:, 0]
-        val_x = self.train_input.iloc[:, 1:]
-        val_y = self.train_input.iloc[:, 0]
-
-        # test handling of group columns
-        with self.assertRaises(ValueError):
-            model_without_group.fit(train_x, train_y)
-
-        with self.assertRaises(ValueError):
-            model.fit(val_x, val_y)
-
-        # test fitting metamodel
-        model.fit(train_x, train_y)
-        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model))
-
-        # test parallel fitting
-        model_parallel.fit(train_x, train_y)
-        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model_parallel))
-
-        # test prediction
-        res = model.predict(train_x)
-        group = train_with_time.iloc[:, -1]
-        for k, estimator in model.estimators_.items():
-            self.assertTrue(
-                (
-                    res[group == k]
-                    == estimator.predict(train_with_time.loc[group == k].iloc[:, 1:-1])
-                ).all()
-            )
-
-        print(train_x.reset_index(drop=True).sample(20).index)
-        print(model.predict(train_x.reset_index(drop=True).sample(20)))
-        self.assertFalse((model.predict(train_x.sample(20)) == np.nan).any())
-
-        # test kwargs in fit and predict methods
-        model_lgb = GroupedRegressor(LGBMRegressor(), group_columns="time")
-        with self.assertRaises(ValueError):
-            model_lgb.fit(train_x, train_y, eval_set=[(val_x, val_y)])
-
-        model_lgb.fit(train_x, train_y, eval_set=[(train_x, train_y)], verbose=False)
-        self.assertIsNone(sklearn.utils.validation.check_is_fitted(model_lgb))
-        res = model_lgb.predict(train_x, raw_score=True)
-        for k, estimator in model_lgb.estimators_.items():
-            self.assertTrue(
-                (
-                    res[group == k]
-                    == estimator.predict(
-                        train_with_time.loc[group == k].iloc[:, 1:-1], raw_score=True
-                    )
-                ).all()
-            )
