95e9c20155bc31141814df5cdfce9defcbf21edb
Benoit Hardier <benoit.hardier@probayes.com>
2022-04-04 16:15:51 +0200

    add test for custom and default backtest
    Signed-off-by: Benoit Hardier <benoit.hardier@probayes.com>

--- test/unit/pipeline/test_train_predict_backtest.py
+++ test/unit/pipeline/test_train_predict_backtest.py
@@ -4,11 +4,26 @@
 from test.unit.utils.base import BaseTestCase
 from test.unit.utils.data import TestData
 
+import numpy as np
+from sklearn.model_selection import TimeSeriesSplit
+from openstef.data_classes.split_function import SplitFuncDataClass
 from openstef.feature_engineering.feature_applicator import TrainFeatureApplicator
 from openstef.pipeline.train_create_forecast_backtest import (
     train_model_and_forecast_back_test,
 )
 from openstef.validation import validation
+
+
+def timeseries_split(data, n_folds, gap):
+    test_fraction = 0.15
+    nb_test = int(np.round(test_fraction * len(data)))
+    test_set = data.iloc[-nb_test:]
+    input_set = data.iloc[:-nb_test]
+    tscv = TimeSeriesSplit(n_splits=n_folds, test_size=nb_test, gap=gap)
+    return [
+        (input_set.iloc[idx_train], input_set.iloc[idx_val], test_set)
+        for idx_train, idx_val in tscv.split(input_set)
+    ]
 
 
 class TestTrainBackTestPipeline(BaseTestCase):
@@ -68,3 +83,77 @@ class TestTrainBackTestPipeline(BaseTestCase):
             horizons=[0.25, 24.0], feature_names=self.modelspecs.feature_names
         ).add_features(validated_data, pj=self.pj)
         self.assertEqual(len(forecast), len(data_with_features))
+
+    def test_train_model_pipeline_core_custom_split(self):
+
+        pj = self.pj
+        # test wrong custom backtest split
+        pj.backtest_split_func = SplitFuncDataClass(
+            function="unknow_backtest", arguments={}
+        )
+        with self.assertRaises(ValueError):
+            (
+                forecast,
+                model,
+                train_data,
+                validation_data,
+                test_data,
+            ) = train_model_and_forecast_back_test(
+                pj=self.pj,
+                modelspecs=self.modelspecs,
+                input_data=self.train_input,
+                training_horizons=[0.25, 24.0],
+                n_folds=4,
+            )
+
+        pj.backtest_split_func = SplitFuncDataClass(
+            function=lambda data: timeseries_split(data, 0, 24), arguments={}
+        )
+        with self.assertRaises(ValueError):
+            (
+                forecast,
+                model,
+                train_data,
+                validation_data,
+                test_data,
+            ) = train_model_and_forecast_back_test(
+                pj=self.pj,
+                modelspecs=self.modelspecs,
+                input_data=self.train_input,
+                training_horizons=[0.25, 24.0],
+                n_folds=4,
+            )
+
+        # test custom backtest split
+        pj.backtest_split_func = SplitFuncDataClass(
+            function=timeseries_split, arguments={"gap": 24}
+        )
+        (
+            forecast,
+            model,
+            train_data,
+            validation_data,
+            test_data,
+        ) = train_model_and_forecast_back_test(
+            pj=self.pj,
+            modelspecs=self.modelspecs,
+            input_data=self.train_input,
+            training_horizons=[0.25, 24.0],
+            n_folds=4,
+        )
+
+        self.assertTrue("forecast" in forecast.columns)
+        self.assertTrue("realised" in forecast.columns)
+        self.assertTrue("horizon" in forecast.columns)
+        self.assertEqual(sorted(list(forecast.horizon.unique())), [0.25, 24.0])
+
+        # check if forecast is indeed of the entire range of the input data
+        test_fraction = 0.15
+        nb_test = int(np.round(test_fraction * len(self.train_input)))
+        validated_data = validation.clean(
+            validation.validate(self.pj["id"], self.train_input.iloc[-nb_test:])
+        )
+        data_with_features = TrainFeatureApplicator(
+            horizons=[0.25, 24.0], feature_names=self.modelspecs.feature_names
+        ).add_features(validated_data, pj=self.pj)
+        self.assertEqual(len(forecast), 4 * len(data_with_features))
