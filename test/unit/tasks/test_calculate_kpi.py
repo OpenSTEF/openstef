3d7e92892b5be3dc473db643922cf3be1d7d8500
Frank Kreuwel <frank.kreuwel@alliander.com>
2022-04-12 15:34:05 +0200

    run black
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>

--- test/unit/tasks/test_calculate_kpi.py
+++ test/unit/tasks/test_calculate_kpi.py
@@ -150,7 +150,7 @@ class TestPerformanceCalcKpiForSpecificPid(BaseTestCase):
             prediction_job["id"], realised_load, predicted_load, basecase=pd.DataFrame()
         )
         # Assert KPIs
-        assert isinstance(kpis  , dict)# TODO improve
+        assert isinstance(kpis, dict)  # TODO improve
 
 
 # Run all tests
