468fcaef5d37c625020d0ede4b43fae131274c95
Alexis Mignon <alexis.mignon@proabyes.com>
2022-04-14 10:13:53 +0200

    Added missing License comment in new test_split_function.py file
    Signed-off-by: Alexis Mignon <alexis.mignon@proabyes.com>

--- test/unit/data_classes/test_split_function.py
+++ test/unit/data_classes/test_split_function.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2022 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 import unittest
 import json
 from openstef.data_classes.split_function import SplitFuncDataClass
