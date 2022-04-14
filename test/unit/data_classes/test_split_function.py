9d377caa40446085ac53c65e71958b9a3211dfa4
Alexis Mignon <alexis.mignon@proabyes.com>
2022-04-14 10:11:25 +0200

    Added specific tests for the SplitFuncDataClass object
    Signed-off-by: Alexis Mignon <alexis.mignon@proabyes.com>

---
+++ test/unit/data_classes/test_split_function.py
@@ -0,0 +1,49 @@
+import unittest
+import json
+from openstef.data_classes.split_function import SplitFuncDataClass
+
+
+def dummy_split_func(arg1, arg2, *args, **kwargs):
+    pass
+
+
+class TestSplitFuncDataClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.arguments = dict(arg1=1, arg2=2, arg3=3)
+        self.split_func_with_strings = SplitFuncDataClass(
+            function="test.unit.data_classes.test_split_function.dummy_split_func",
+            arguments=json.dumps(self.arguments),
+        )
+        self.split_func_with_objects = SplitFuncDataClass(
+            function=dummy_split_func, arguments=self.arguments
+        )
+
+    def test_getattr(self):
+        self.assertIs(
+            self.split_func_with_objects["function"],
+            dummy_split_func,
+        )
+
+        self.assertEqual(self.split_func_with_objects["arguments"], self.arguments)
+
+    def test_load_function(self):
+        split_func, args = self.split_func_with_objects.load()
+        self.assertIs(split_func, dummy_split_func)
+        self.assertEqual(args, self.arguments)
+
+        split_func, args = self.split_func_with_strings.load()
+        self.assertIs(split_func, dummy_split_func)
+        self.assertEqual(args, self.arguments)
+
+    def test_check_arguments(self):
+        # Should not raise exception
+        _ = self.split_func_with_strings.load(required_arguments=["arg1", "arg2"])
+
+        with self.assertRaises(ValueError):
+            self.split_func_with_strings.load(
+                required_arguments=["arg1", "arg2", "arg3"]
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
