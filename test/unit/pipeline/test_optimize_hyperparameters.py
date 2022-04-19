fdaf6bf5d35dd347772afde23bc099e49c7b2b88
David Swinkels <david.swinkels@alliander.com>
2022-04-19 09:49:39 +0200

    Setup initial cleanup of reporter and mlflow model code
    Signed-off-by: David Swinkels <david.swinkels@alliander.com>

--- test/unit/pipeline/test_optimize_hyperparameters.py
+++ test/unit/pipeline/test_optimize_hyperparameters.py
@@ -1,7 +1,6 @@
 # SPDX-FileCopyrightText: 2017-2022 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-import unittest
 from test.unit.utils.base import BaseTestCase
 from test.unit.utils.data import TestData
 from unittest.mock import patch
