a1f8acdf6e59b33d4b7c5a52fddfcecefae87f63
David Swinkels <david.swinkels@alliander.com>
2022-05-09 10:35:24 +0200

    Test new release
    Signed-off-by: David Swinkels <david.swinkels@alliander.com>

--- blub.py
+++ blub.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2017-2022 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-test = "test4"
+test = "test5"
 print(test)
