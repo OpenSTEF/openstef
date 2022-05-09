774e7af4b6adf3e16096148a16f84283963f5016
David Swinkels <david.swinkels@alliander.com>
2022-05-09 10:25:15 +0200

    Update release version

--- blub.py
+++ blub.py
@@ -1,5 +1,5 @@
 # SPDX-FileCopyrightText: 2017-2022 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-test = "test2"
+test = "test3"
 print(test)
