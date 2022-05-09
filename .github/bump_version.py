e5514597e463213da8f95986d4ec45e1ca5fd3a4
David Swinkels <david.swinkels@alliander.com>
2022-05-09 08:53:36 +0200

    Add bump version and make release with new version
    Signed-off-by: David Swinkels <david.swinkels@alliander.com>

--- .github/bump_version.py
+++ .github/bump_version.py
@@ -4,22 +4,18 @@
 
 import re
 
-with open("setup.py", "r") as file:
+with open('setup.py', "r") as file:
     version_content = file.read()
-
-# Match regex for version=0.0.0a, pattern
-minor_version_number_with_suffix = re.findall(
-    r'version="\d+\.\d+\.([a-zA-Z0-9]+)",', version_content
-)[0]
-minior_version_number = int(re.findall(r"\d+", minor_version_number_with_suffix)[0])
-regex_bumped_minor_version_number = f"\g<1>{minior_version_number + 1}"
-
-# Match regex for version=0.0.0a pattern
-bumped_version_content = re.sub(
-    r'(version="\d+\.\d+\.)[a-zA-Z0-9]+',
-    regex_bumped_minor_version_number,
-    version_content,
-)
-
-with open("setup.py", "w") as file:
+# Match regex for version="0.0.0a",, pattern
+old_semantic_version = re.findall(r'version="(\d+\.\d+\.[a-zA-Z0-9]+)",',
+                                  version_content)
+major_version, minor_version, patch_version = old_semantic_version[0].split(".")
+patch_version = int(re.findall(r'\d+', patch_version)[0])
+new_semantic_version = f"{major_version}.{minor_version}.{patch_version + 1}"
+regex_bumped_patch_version = f'\g<1>{new_semantic_version}'
+# Match regex for version="0.0.0a pattern
+bumped_version_content = re.sub(r'(version=")\d+\.\d+\.[a-zA-Z0-9]+',
+                                regex_bumped_patch_version, version_content)
+with open('setup.py', "w") as file:
     file.write(bumped_version_content)
+print(new_semantic_version)
