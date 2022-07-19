2dfc65e3462fe73da035829f09d426998158c885
Frank Kreuwel <fpmkreuwel@gmail.com>
2022-07-19 16:41:25 +0200

    skip proloaf in unit test
    Signed-off-by: Frank Kreuwel <fpmkreuwel@gmail.com>

--- openstef/enums.py
+++ openstef/enums.py
@@ -3,14 +3,25 @@
 # SPDX-License-Identifier: MPL-2.0
 from enum import Enum
 
+# Specify optional modeltypes
+try:
+    from openstef.model.regressors.proloaf import OpenstfProloafRegressor
+
+    _has_proloaf = True
+except ImportError:
+    _has_proloaf = False
+
 
 # TODO replace this with ModelType (MLModelType == Machine Learning model type)
 class MLModelType(Enum):
     XGB = "xgb"
     XGB_QUANTILE = "xgb_quantile"
     LGB = "lgb"
-    ProLoaf = "proloaf"
     LINEAR = "linear"
+    if _has_proloaf:
+        ProLoaf = "proloaf"
+    else:
+        ProLoaf = None
 
 
 class ForecastType(Enum):
