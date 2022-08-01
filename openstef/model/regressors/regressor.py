33760f9237fe05def545fe62af7d2f62539b8c61
Frank Kreuwel <fpmkreuwel@gmail.com>
2022-08-01 16:49:07 +0200

    remove unused import
    Signed-off-by: Frank Kreuwel <fpmkreuwel@gmail.com>

--- openstef/model/regressors/regressor.py
+++ openstef/model/regressors/regressor.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 import numpy as np
 import pandas as pd
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import RegressorMixin
 
 from openstef.model.regressors.regressor_interface import OpenstfRegressorInterface
 
