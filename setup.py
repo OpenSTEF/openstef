03a76be9ba9e794aedf6c1b0b0efdee3f34bfac8
bump_version <action@github.com>
2022-10-31 14:34:46 +0000

    Bumped minor version
    Signed-off-by: bump_version <action@github.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def read_long_description_from_readme():
 
 setup(
     name="openstef",
-    version="3.2.28",
+    version="3.2.29",
     packages=find_packages(include=["openstef", "openstef.*"]),
     description="Open short term energy forecaster",
     long_description=read_long_description_from_readme(),
