e1d477312338426f5f1031a76203ec746b16d788
Benoit Hardier <benoit.hardier@probayes.com>
2022-11-21 12:50:38 +0100

    unit test for forecast_pid
    Signed-off-by: Benoit Hardier <benoit.hardier@probayes.com>

--- test/unit/pipeline/test_pipeline_create_forecast.py
+++ test/unit/pipeline/test_pipeline_create_forecast.py
@@ -7,7 +7,8 @@ from datetime import datetime as dt
 from pathlib import Path
 from test.unit.utils.base import BaseTestCase
 from test.unit.utils.data import TestData
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+from mlflow.exceptions import MlflowException
 
 from openstef.model.serializer import MLflowSerializer
 from openstef.pipeline import create_forecast, utils
@@ -194,3 +195,53 @@ class TestCreateForecastPipeline(BaseTestCase):
         self.assertEqual(len(forecast.columns), 15)
         self.assertGreater(forecast.forecast.min(), -5)
         self.assertLess(forecast.forecast.max(), 85)
+
+    @patch("openstef.model.serializer.MLflowSerializer.load_model")
+    @patch("openstef.pipeline.create_forecast.create_forecast_pipeline_core")
+    def test_create_forecast_pipeline_wrong_forecast_pid(
+        self, create_forecast_pipeline_core_mock, load_mock
+    ):
+        """Test the forecast_pid parameter of th pj with a wrong pid."""
+
+        def side_effects(experiment_name):
+            if experiment_name == "307":
+                return self.model, self.model_specs
+            raise MlflowException("Wrong pid")
+
+        load_mock.side_effect = side_effects
+        create_forecast_pipeline_core_mock.return_value = MagicMock()
+
+        self.pj.forecast_pid = "703"
+        # Load prediction job and forecast data
+        forecast_data = self.data
+        col_name = forecast_data.columns[0]
+        forecast_data.loc["2020-11-28 00:00:00":"2020-12-01", col_name] = None
+        with self.assertRaises(MlflowException):
+            create_forecast.create_forecast_pipeline(
+                self.pj, forecast_data, "./test/unit/trained_models/mlruns"
+            )
+
+    @patch("openstef.model.serializer.MLflowSerializer.load_model")
+    @patch("openstef.pipeline.create_forecast.create_forecast_pipeline_core")
+    def test_create_forecast_pipeline_valid_forecast_pid(
+        self, create_forecast_pipeline_core_mock, load_mock
+    ):
+        """Test the forecast_pid parameter of th pj with a valid pid."""
+
+        def side_effects(experiment_name):
+            if experiment_name == "3070":
+                return self.model, self.model_specs
+            raise MlflowException("Wrong pid")
+
+        load_mock.side_effect = side_effects
+        create_forecast_pipeline_core_mock.return_value = MagicMock()
+
+        self.pj.forecast_pid = "3070"
+        # Load prediction job and forecast data
+        forecast_data = self.data
+        col_name = forecast_data.columns[0]
+        forecast_data.loc["2020-11-28 00:00:00":"2020-12-01", col_name] = None
+        create_forecast.create_forecast_pipeline(
+            self.pj, forecast_data, "./test/unit/trained_models/mlruns"
+        )
+        self.assertTrue(create_forecast_pipeline_core_mock.called)
