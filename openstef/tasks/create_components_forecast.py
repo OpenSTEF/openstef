f45ed1b854effc4be706c37c88d3f1190b1d65b1
Frank Kreuwel <frank.kreuwel@alliander.com>
2023-03-01 11:20:29 +0100

    improve error (#438)
    * improve error
    
    * Format Python code with Black
    
    Signed-off-by: black <action@github.com>
    
    * Update openstef/tasks/create_components_forecast.py
    
    Co-authored-by: Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
    Signed-off-by: Frank Kreuwel <fpmkreuwel@gmail.com>
    
    * Format Python code with Black
    
    Signed-off-by: black <action@github.com>
    
    * Update create_components_forecast.py
    
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    
    ---------
    
    Signed-off-by: black <action@github.com>
    Signed-off-by: Frank Kreuwel <fpmkreuwel@gmail.com>
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    Co-authored-by: Frank Kreuwel <fpmkreuwel@gmail.com>
    Co-authored-by: black <action@github.com>
    Co-authored-by: Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>

--- openstef/tasks/create_components_forecast.py
+++ openstef/tasks/create_components_forecast.py
@@ -26,6 +26,7 @@ from pathlib import Path
 
 import pytz
 import structlog
+import pandas as pd
 
 from openstef.data_classes.prediction_job import PredictionJobDataClass
 from openstef.enums import MLModelType
@@ -92,6 +93,11 @@ def create_components_forecast_task(
 
     # Make forecast for the demand, wind and pv components
     forecasts = create_components_forecast_pipeline(pj, input_data, weather_data)
+
+    if not isinstance(forecasts.index, pd.core.indexes.datetimes.DatetimeIndex):
+        raise ValueError(
+            f"Index is not datetime. Received forecasts:{forecasts.head()}"
+        )
 
     if forecasts.index.max() < datetime.utcnow().replace(tzinfo=pytz.utc) + timedelta(
         hours=30
