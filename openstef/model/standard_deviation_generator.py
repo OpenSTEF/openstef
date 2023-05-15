d45affced285749b3ff23add530b89eb751a75f1
Frank Kreuwel <frank.kreuwel@alliander.com>
2023-05-15 09:04:02 +0200

    Bugfix/ktps 2838 stdev explodes (#462)
    * improves stdev calculation
    
    * test and cap stdev extrapolation
    
    * Format Python code with Black
    
    Signed-off-by: black <action@github.com>
    
    * Update confidence_interval_applicator.py
    
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    
    * Update confidence_interval_applicator.py
    
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    
    * Update confidence_interval_applicator.py
    
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    
    * Format Python code with Black
    
    Signed-off-by: black <action@github.com>
    
    ---------
    
    Signed-off-by: black <action@github.com>
    Signed-off-by: Frank Kreuwel <frank.kreuwel@alliander.com>
    Co-authored-by: black <action@github.com>

--- openstef/model/standard_deviation_generator.py
+++ openstef/model/standard_deviation_generator.py
@@ -73,9 +73,7 @@ class StandardDeviationGenerator:
         # For the time starts with 00, 01, 02, etc. TODO (MAKE MORE ELEGANT SOLUTION THAN A LOOP)
         for hour in range(24):
             hour_error = error[error.index == hour]
-            result["stdev"].iloc[hour] = np.std(
-                hour_error.iloc[1:]
-            )  # Exclude first item as this is the hour itself!
+            result["stdev"].iloc[hour] = np.std(hour_error)
             result["hour"].iloc[hour] = hour
 
         result = result.astype("float")
