5343489a145cf32b6ac9190730626bc5fdbdd3e7
Bart Pleiter <bart.pleiter@alliander.com>
2023-10-30 16:08:46 +0100

    Added freeze_time for flatliner unit tests. Improved  datetime comparison.
    Signed-off-by: Bart Pleiter <bart.pleiter@alliander.com>

--- test/unit/validation/test_validation_detect_ongoing_zero_flatliner.py
+++ test/unit/validation/test_validation_detect_ongoing_zero_flatliner.py
@@ -3,26 +3,28 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from datetime import datetime, timedelta
+from freezegun import freeze_time
 from test.unit.utils.base import BaseTestCase
 import numpy as np
 import pandas as pd
 
 from openstef.validation.validation import detect_ongoing_zero_flatliner
 
-now = datetime.utcnow()
-three_hour_range = pd.date_range(
-    start=now - timedelta(minutes=180), end=now, freq="0.25H"
-)
-
-four_hour_range_predict_setting = pd.date_range(
-    start=now - timedelta(minutes=180), end=now + timedelta(minutes=60), freq="0.25H"
-)
-
-
+@freeze_time("2023-10-30 12:01:02")
 class TestDetectOngoingZeroFlatliners(BaseTestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        now = datetime.utcnow()
+        self.three_hour_range = pd.date_range(
+            start=now - timedelta(minutes=180), end=now, freq="0.25H"
+        )
+        self.four_hour_range_predict_setting = pd.date_range(
+            start=now - timedelta(minutes=180), end=now + timedelta(minutes=60), freq="0.25H"
+        )
+        
     def test_all_zero(self):
         # Arrange
-        load = pd.Series(index=three_hour_range, data=[0 for i in range(13)])
+        load = pd.Series(index=self.three_hour_range, data=[0 for i in range(13)])
         duration_threshold = 120
 
         # Act
@@ -33,7 +35,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
 
     def test_all_nonzero(self):
         # Arrange
-        load = pd.Series(index=three_hour_range, data=[i for i in range(1, 14)])
+        load = pd.Series(index=self.three_hour_range, data=[i for i in range(1, 14)])
         duration_threshold = 120
 
         # Act
@@ -47,7 +49,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
         # now the pattern has ended since the last measurement is not zero anymore.
 
         # Arrange
-        load = pd.Series(index=three_hour_range, data=[0 for i in range(1, 13)] + [1])
+        load = pd.Series(index=self.three_hour_range, data=[0 for i in range(1, 13)] + [1])
         duration_threshold = 120
 
         # Act
@@ -62,7 +64,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
 
         # Arrange
         load = pd.Series(
-            index=three_hour_range, data=[i for i in range(1, 10)] + [0, 0, 0, 0]
+            index=self.three_hour_range, data=[i for i in range(1, 10)] + [0, 0, 0, 0]
         )
         duration_threshold = 120
 
@@ -75,7 +77,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
     def test_zero_flatliner_pattern_just_above_threshold(self):
         # Arrange
         load = pd.Series(
-            index=three_hour_range, data=[1, 2, 3, 4] + [0 for i in range(9)]
+            index=self.three_hour_range, data=[1, 2, 3, 4] + [0 for i in range(9)]
         )
         duration_threshold = 120
 
@@ -88,7 +90,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
     def test_zero_flatliner_and_missing_values(self):
         # Arrange
         load = pd.Series(
-            index=three_hour_range,
+            index=self.three_hour_range,
             data=[1, 2, 3, 4] + [0, 0, 0, 0, np.nan, np.nan, np.nan, np.nan, 0],
         )
         duration_threshold = 120
@@ -101,7 +103,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
 
     def test_all_missing_values(self):
         # Arrange
-        load = pd.Series(index=three_hour_range, data=[np.nan for i in range(13)])
+        load = pd.Series(index=self.three_hour_range, data=[np.nan for i in range(13)])
         duration_threshold = 120
 
         # Act
@@ -117,7 +119,7 @@ class TestDetectOngoingZeroFlatliners(BaseTestCase):
 
         # Arrange
         load = pd.Series(
-            index=four_hour_range_predict_setting,
+            index=self.four_hour_range_predict_setting,
             data=[1, 2, 3]
             + [0, 0, 0, 0, 0, 0, 0, 0, 0]
             + [np.nan, np.nan, np.nan, np.nan, np.nan],
