316e8c7d1bba98f29935c9ead027ea0d8ca99576
Bart Pleiter <bart.pleiter@alliander.com>
2024-01-15 15:44:09 +0100

    Fixed replacing of repeated values.
    Signed-off-by: Bart Pleiter <bart.pleiter@alliander.com>

--- test/unit/preprocessing/test_preprocessing.py
+++ test/unit/preprocessing/test_preprocessing.py
@@ -2,10 +2,8 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-import math
 from test.unit.utils.base import BaseTestCase
 
-import numpy as np
 import pandas as pd
 
 from openstef.preprocessing import preprocessing
@@ -13,44 +11,36 @@ from openstef.preprocessing import preprocessing
 
 class TestPreprocessing(BaseTestCase):
     def test_replace_repeated_values_with_nan(self):
-        start_nan = 8
-        end_nan = 11
-        data = np.arange(20)
-        df = pd.DataFrame({"Column1": data[:], "Column2": data[:], "Column3": data[:]})
-        # Add 4 sequentially same numbers
-        df["Column2"] = np.where(
-            df["Column2"].between(start_nan, end_nan), 30, df["Column2"]
+        # Arrange
+        data_no_repeated_sequences = [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
+        data_two_repeated_sequences = [0, 1, 2, 2, 2, 2, 3, 4, 2, 2, 2, 5]
+
+        df = pd.DataFrame(
+            {
+                "Column1": data_no_repeated_sequences,
+                "Column2": data_two_repeated_sequences,
+            }
         )
 
-        # No change, since there are 4 seq. numbers.
-        df_no_repeated = preprocessing.replace_repeated_values_with_nan(
-            df, 5, "Column2"
+        # Act
+        df_no_repeated_sequences = preprocessing.replace_repeated_values_with_nan(
+            df, 2, "Column1"
         )
-        self.assertFalse(df_no_repeated.isna().values.any())
 
-        # No change, since there are 4 seq. numbers.
-        df_no_repeated = preprocessing.replace_repeated_values_with_nan(
+        df_repeated_sequences_below_threshold = (
+            preprocessing.replace_repeated_values_with_nan(df, 8, "Column2")
+        )
+
+        df_one_repeated_sequence = preprocessing.replace_repeated_values_with_nan(
             df, 4, "Column2"
         )
-        self.assertFalse(df_no_repeated.isna().values.any())
 
-        # Change, since there are 4 seq. numbers and we don't allow for more than 3
-        df_no_repeated = preprocessing.replace_repeated_values_with_nan(
+        df_two_repeated_sequences = preprocessing.replace_repeated_values_with_nan(
             df, 3, "Column2"
         )
-        self.assertEqual(df_no_repeated["Column1"].isna().values.sum(), 0)
-        self.assertEqual(df_no_repeated["Column2"].isna().values.sum(), 1)
-        self.assertEqual(df_no_repeated["Column3"].isna().values.sum(), 0)
-        self.assertTrue(df_no_repeated["Column1"].equals(df_no_repeated["Column3"]))
-        self.assertTrue(math.isnan(df_no_repeated.at[end_nan, "Column2"]))
 
-        # Change, since there are 4 seq. numbers and we don't allow for more than 2
-        df_no_repeated = preprocessing.replace_repeated_values_with_nan(
-            df, 2, "Column2"
-        )
-        self.assertEqual(df_no_repeated["Column1"].isna().values.sum(), 0)
-        self.assertEqual(df_no_repeated["Column2"].isna().values.sum(), 2)
-        self.assertEqual(df_no_repeated["Column3"].isna().values.sum(), 0)
-        self.assertTrue(df_no_repeated["Column1"].equals(df_no_repeated["Column3"]))
-        self.assertTrue(math.isnan(df_no_repeated.at[end_nan, "Column2"]))
-        self.assertTrue(math.isnan(df_no_repeated.at[end_nan - 1, "Column2"]))
+        # Assert
+        self.assertFalse(df_no_repeated_sequences.isna().values.any())
+        self.assertFalse(df_repeated_sequences_below_threshold.isna().values.any())
+        self.assertEqual(df_one_repeated_sequence["Column2"].isna().values.sum(), 4)
+        self.assertEqual(df_two_repeated_sequences["Column2"].isna().values.sum(), 7)
