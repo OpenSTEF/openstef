d65dc4bc93479909068fe00925509927858e958f
Bart Pleiter <bart.pleiter@alliander.com>
2024-01-16 10:42:14 +0100

    Split preprocessing unit test.
    Signed-off-by: Bart Pleiter <bart.pleiter@alliander.com>

--- test/unit/preprocessing/test_preprocessing.py
+++ test/unit/preprocessing/test_preprocessing.py
@@ -12,6 +12,30 @@ from openstef.preprocessing import preprocessing
 class TestPreprocessing(BaseTestCase):
     def test_replace_repeated_values_with_nan(self):
         # Arrange
+        data_two_repeated_sequences = [0, 1, 2, 2, 2, 2, 3, 4, 2, 2, 2, 5]
+
+        df = pd.DataFrame({"Column1": data_two_repeated_sequences})
+
+        # Act
+        df_repeated_sequences_below_threshold = (
+            preprocessing.replace_repeated_values_with_nan(df, 8, "Column1")
+        )
+
+        df_one_repeated_sequence = preprocessing.replace_repeated_values_with_nan(
+            df, 4, "Column1"
+        )
+
+        df_two_repeated_sequences = preprocessing.replace_repeated_values_with_nan(
+            df, 3, "Column1"
+        )
+
+        # Assert
+        self.assertFalse(df_repeated_sequences_below_threshold.isna().values.any())
+        self.assertEqual(df_one_repeated_sequence.isna().values.sum(), 4)
+        self.assertEqual(df_two_repeated_sequences.isna().values.sum(), 7)
+
+    def test_replace_repeated_values_with_nan_ignore_no_repetition(self):
+        # Arrange
         data_no_repeated_sequences = [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]
         data_two_repeated_sequences = [0, 1, 2, 2, 2, 2, 3, 4, 2, 2, 2, 5]
 
@@ -27,20 +51,5 @@ class TestPreprocessing(BaseTestCase):
             df, 2, "Column1"
         )
 
-        df_repeated_sequences_below_threshold = (
-            preprocessing.replace_repeated_values_with_nan(df, 8, "Column2")
-        )
-
-        df_one_repeated_sequence = preprocessing.replace_repeated_values_with_nan(
-            df, 4, "Column2"
-        )
-
-        df_two_repeated_sequences = preprocessing.replace_repeated_values_with_nan(
-            df, 3, "Column2"
-        )
-
         # Assert
         self.assertFalse(df_no_repeated_sequences.isna().values.any())
-        self.assertFalse(df_repeated_sequences_below_threshold.isna().values.any())
-        self.assertEqual(df_one_repeated_sequence["Column2"].isna().values.sum(), 4)
-        self.assertEqual(df_two_repeated_sequences["Column2"].isna().values.sum(), 7)
