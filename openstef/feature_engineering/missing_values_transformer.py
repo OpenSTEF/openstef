94b8a60b889f7632551299d76534786ee6c98fc2
Egor Dmitriev <egor.dmitriev@luminis.eu>
2024-03-29 13:24:18 +0100

    fix(KTP-762): Added license
    Signed-off-by: Egor Dmitriev <egor.dmitriev@luminis.eu>

--- openstef/feature_engineering/missing_values_transformer.py
+++ openstef/feature_engineering/missing_values_transformer.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
 from typing import Union, List
 
 import numpy as np
