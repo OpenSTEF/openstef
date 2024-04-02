feature(KTP-762): Changed load/y normalization range to (-1,1). Updated documentation for MissingValueTransformer and LinearQuantile. Applied code fix suggestions from Sonarcloud.
Signed-off-by: Egor Dmitriev <egor.dmitriev@luminis.eu>

openstef/feature_engineering/missing_values_transformer.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
 #
 # SPDX-License-Identifier: MPL-2.0
-from typing import Union, List
+from typing import Union, List, Optional
 
 import numpy as np
 import pandas as pd
@@ -11,8 +11,13 @@ from sklearn.utils.validation import check_array
 
 
 class MissingValuesTransformer:
-    in_feature_names: List[str] = None
-    _n_in_features: int = None
+    """
+    MissingValuesTransformer handles missing values in data by imputing them with a
+    given strategy. It also removes columns that are always null from the data.
+    """
+
+    in_feature_names: Optional[List[str]] = None
+    _n_in_features: Optional[int] = None
 
     non_null_feature_names: List[str] = None
 
@@ -22,14 +27,27 @@ class MissingValuesTransformer:
         imputation_strategy: str = None,
         fill_value: Union[str, int, float] = None,
     ):
-        """Initialize missing values handler."""
+        """
+        Initialize missing values handler.
+
+        Args:
+            missing_values: The placeholder for the missing values. All occurrences of
+                `missing_values` will be imputed.
+            imputation_strategy: The imputation strategy to use
+                Can be one of "mean", "median", "most_frequent", "constant" or None.
+            fill_value: When strategy == "constant", fill_value is used to replace all
+                occurrences of missing_values.
+        """
         self.missing_values = missing_values
         self.imputation_strategy = imputation_strategy
         self.fill_value = fill_value
 
     def fit(self, x, y=None):
+        """
+        Fit the imputer on the input data.
+        """
         _ = check_array(x, force_all_finite="allow-nan")
-        if type(x) != pd.DataFrame:
+        if not isinstance(x, pd.DataFrame):
             x = pd.DataFrame(np.asarray(x))
 
         self.in_feature_names = list(x.columns)
@@ -55,8 +73,11 @@ class MissingValuesTransformer:
         self.imputer_.fit(X=x, y=None)
 
     def transform(self, x) -> pd.DataFrame:
+        """
+        Transform the input data by imputing missing values.
+        """
         _ = check_array(x, force_all_finite="allow-nan")
-        if type(x) != pd.DataFrame:
+        if not isinstance(x, pd.DataFrame):
             x = pd.DataFrame(np.asarray(x))
 
         x = x[self.non_null_feature_names]
@@ -64,9 +85,18 @@ class MissingValuesTransformer:
         return self.imputer_.transform(x)
 
     def fit_transform(self, x, y=None):
+        """
+        Fit the imputer on the input data and transform it.
+
+        Returns:
+            The data with missing values imputed.
+        """
         self.fit(x, y)
         return self.transform(x)
 
     @classmethod
     def _identity(cls, x):
         return x
+
+    def __sklearn_is_fitted__(self) -> bool:
+        return self.in_feature_names is not None

openstef/model/regressors/linear_quantile.py
@@ -27,7 +27,7 @@ class LinearQuantileOpenstfRegressor(OpenstfRegressor, RegressorMixin):
     imputer_: MissingValuesTransformer
     x_scaler_: MinMaxScaler
     y_scaler_: MinMaxScaler
-    models_: Dict[float, QuantileRegressor] = None
+    models_: Dict[float, QuantileRegressor]
 
     is_fitted_: bool = False
 
@@ -53,6 +53,12 @@ class LinearQuantileOpenstfRegressor(OpenstfRegressor, RegressorMixin):
         Model that provides quantile regression with SKLearn QuantileRegressor.
         For each desired quantile an QuantileRegressor model is trained,
         these can later be used to predict quantiles.
+
+        This model is sensitive to feature quality and therefore has logic to remove
+        some custom features produced by OpenSTEF. The features that are removed are:
+        - Holiday features (is_christmas, is_*)
+        - Lagged features (T-1d, T-*)
+        - Point in time features (IsWeekendDay, IsWeekDay, IsSunday, Month, Quarter)
 
         Args:
             quantiles: Tuple with desired quantiles, quantile 0.5 is required.
@@ -81,7 +87,7 @@ class LinearQuantileOpenstfRegressor(OpenstfRegressor, RegressorMixin):
             fill_value=fill_value,
         )
         self.x_scaler_ = MinMaxScaler(feature_range=(-1, 1))
-        self.y_scaler_ = MinMaxScaler(feature_range=(0, 1))
+        self.y_scaler_ = MinMaxScaler(feature_range=(-1, 1))
         self.models_ = {
             quantile: QuantileRegressor(alpha=alpha, quantile=quantile, solver=solver)
             for quantile in quantiles
@@ -122,7 +128,7 @@ class LinearQuantileOpenstfRegressor(OpenstfRegressor, RegressorMixin):
             # Ignore holiday features
             re.match(r"is_", feature_name) is not None
             or
-            # Ignore log features
+            # Ignore lag features
             re.match(r"T-", feature_name) is not None
         )
 
@@ -148,7 +154,7 @@ class LinearQuantileOpenstfRegressor(OpenstfRegressor, RegressorMixin):
             Fitted LinearQuantile model
 
         """
-        if type(y) != pd.Series:
+        if not isinstance(y, pd.Series):
             y = pd.Series(np.asarray(y), name="load")
 
         x = self._remove_ignored_features(x)