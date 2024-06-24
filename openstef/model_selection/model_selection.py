cf1fad8c3dd9ca57a42bc6be95a605211f7982ef
Egor Dmitriev <egor.dmitriev@alliander.com>
2024-06-24 13:58:25 +0200

    fix: Fixed training pipeline typehints.
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>

--- openstef/model_selection/model_selection.py
+++ openstef/model_selection/model_selection.py
@@ -106,7 +106,7 @@ def split_data_train_validation_test(
     validation_fraction: float = 0.15,
     back_test: bool = False,
     stratification_min_max: bool = True,
-) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """Split input data into train, test and validation set.
 
     Function for splitting data with features in a train, test and
