ca5f858acd986ec1eceb20e106f4f7b004a79d6f
lschilders <lars.schilders@alliander.com>
2024-09-27 14:29:13 +0200

    add unit test test_no_imputation_for_future_data
    Signed-off-by: lschilders <lars.schilders@alliander.com>

--- test/unit/feature_engineering/test_missing_values_transformer.py
+++ test/unit/feature_engineering/test_missing_values_transformer.py
@@ -15,41 +15,46 @@ from openstef.feature_engineering.missing_values_transformer import (
 class MissingValuesTransformerTests(BaseTestCase):
     def setUp(self):
         self.data = pd.DataFrame(
-            {"A": [1, np.nan, 3], "B": [4, 5, np.nan], "C": [np.nan, np.nan, np.nan]}
+            {"A": [np.nan, 2, 3], "B": [1, np.nan, 3], "C": [4, 5, np.nan], "D": [np.nan, np.nan, np.nan]}
         )
 
     def test_imputation_with_mean_strategy_fills_missing_values(self):
         transformer = MissingValuesTransformer(imputation_strategy="mean")
         transformed = transformer.fit_transform(self.data)
-        self.assertEqual(transformed.isnull().sum().sum(), 0)
-        self.assertAlmostEqual(transformed.loc[1, "A"], 2)
-        self.assertAlmostEqual(transformed.loc[2, "B"], 4.5)
+        self.assertEqual(transformed[["A", "B"]].isnull().sum().sum(), 0)
+        self.assertAlmostEqual(transformed.loc[0, "A"], 2.5)
+        self.assertAlmostEqual(transformed.loc[1, "B"], 2)
 
     def test_imputation_with_constant_strategy_fills_missing_values(self):
         transformer = MissingValuesTransformer(
             imputation_strategy="constant", fill_value=0
         )
         transformed = transformer.fit_transform(self.data)
-        self.assertEqual(transformed.isnull().sum().sum(), 0)
-        self.assertEqual(transformed.loc[1, "A"], 0)
-        self.assertEqual(transformed.loc[2, "B"], 0)
+        self.assertEqual(transformed[["A", "B"]].isnull().sum().sum(), 0)
+        self.assertEqual(transformed.loc[0, "A"], 0)
+        self.assertEqual(transformed.loc[1, "B"], 0)
 
     def test_columns_always_null_are_removed(self):
         transformer = MissingValuesTransformer()
         transformer.fit(self.data)
-        self.assertNotIn("C", transformer.non_null_feature_names)
+        self.assertNotIn("D", transformer.non_null_feature_names)
 
     def test_non_dataframe_input_is_converted_and_processed(self):
         transformer = MissingValuesTransformer(imputation_strategy="mean")
-        array = np.array([[1, np.nan], [np.nan, 2]])
+        array = np.array([[1, np.nan, np.nan], [np.nan, 2, np.nan]])
         transformed = transformer.fit_transform(array)
         self.assertIsInstance(transformed, pd.DataFrame)
-        self.assertEqual(transformed.isnull().sum().sum(), 0)
+        self.assertEqual(transformed.isnull().sum().sum(), 1)
 
     def test_fitting_transformer_without_strategy_keeps_data_unchanged(self):
         transformer = MissingValuesTransformer()
         transformed = transformer.fit_transform(self.data)
-        pd.testing.assert_frame_equal(transformed, self.data.drop(columns=["C"]))
+        pd.testing.assert_frame_equal(transformed, self.data.drop(columns=["D"]))
+
+    def test_no_imputation_for_future_data(self):
+        transformer = MissingValuesTransformer(imputation_strategy="mean")
+        transformed = transformer.fit_transform(self.data)
+        self.assertIsNAN(transformed.loc[2, "C"])
 
     def test_calling_transform_before_fit_raises_error(self):
         transformer = MissingValuesTransformer()
