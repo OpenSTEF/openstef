135e8d35c5ada052a443007fc08c406c938895b2
Jan Maarten van Doorn <janmaarten.vandoorn@sigholm.se>
2024-11-21 14:25:26 +0100

    add license info

--- openstef/feature_engineering/bidding_zone_to_country_mapping.py
+++ openstef/feature_engineering/bidding_zone_to_country_mapping.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
 BIDDING_ZONE_TO_COUNTRY_CODE_MAPPING = {
     "DE_50HZ": "DE",
     "AL": "AL",
