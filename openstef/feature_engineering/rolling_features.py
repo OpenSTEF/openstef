0b31acdddb7a6556b0578889f10a65d0b6272a4d
lschilders <lars.schilders@alliander.com>
2025-02-05 10:15:47 +0100

    add licenses
    Signed-off-by: lschilders <lars.schilders@alliander.com>

--- openstef/feature_engineering/rolling_features.py
+++ openstef/feature_engineering/rolling_features.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2023 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 import pandas as pd
 
 
