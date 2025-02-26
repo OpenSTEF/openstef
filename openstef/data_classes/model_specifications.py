394b201f17acaeff3001a02891a54c0e60aa6c15
Jan Maarten van Doorn <52956303+JanMaartenvanDoorn@users.noreply.github.com>
2025-02-26 10:45:18 +0100

    updated dataclasses to pydantic v2 models (#594)
    * updated dataclasses to pydantic v2 models
    
    * fix reuse issue
    
    * upgrade split function
    
    * remove v2 option
    
    * remove license due to REUSE fix in #595
    
    * add missing docstrings

--- openstef/data_classes/model_specifications.py
+++ openstef/data_classes/model_specifications.py
@@ -2,27 +2,31 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 """Specifies the dataclass for model specifications."""
-from typing import Optional, Union
 
-from pydantic.v1 import BaseModel
+from typing import Any, Optional, Union
+
+from pydantic import BaseModel, Field
 
 
 class ModelSpecificationDataClass(BaseModel):
     """Holds all information regarding the training procces of a specific model."""
 
-    id: Union[int, str]
-    hyper_params: Optional[dict] = {}
-    """Hyperparameters that should be used during training."""
-    feature_names: Optional[list] = None
-    """Features that should be used during training."""
-    feature_modules: Optional[list] = []
-    """Feature modules that should be used during training."""
+    id: Union[int, str] = Field(description="The model id.")
+    hyper_params: Optional[dict] = Field(
+        default={}, description="Hyperparameters that should be used during training."
+    )
+    feature_names: Optional[list] = Field(
+        default=None, description="Features that should be used during training."
+    )
+    feature_modules: Optional[list] = Field(
+        default=[], description="Modules that should be used during training."
+    )
 
-    def __getitem__(self, item: str) -> any:
+    def __getitem__(self, item: str) -> Any:
         """Allows us to use subscription to get the items from the object."""
         return getattr(self, item)
 
-    def __setitem__(self, key: str, value: any) -> None:
+    def __setitem__(self, key: str, value: Any) -> None:
         """Allows us to use subscription to set the items in the object."""
         if hasattr(self, key):
             self.__dict__[key] = value
