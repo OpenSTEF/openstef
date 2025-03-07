b9e85f1ad2b0d88c78df1855b6767d1691e3a59b
bartpleiter <a.t.pleiter@gmail.com>
2025-03-07 16:06:16 +0100

    fix: move logger type to separate file to prevent circular imports.
    Signed-off-by: bartpleiter <a.t.pleiter@gmail.com>

--- openstef/logging/logger_factory.py
+++ openstef/logging/logger_factory.py
@@ -1,18 +1,7 @@
-# SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
-#
-# SPDX-License-Identifier: MPL-2.0
-
-from enum import StrEnum
-
+from openstef.logging.logger_types import LoggerType
 from openstef.logging.standard_logger import StandardLogger
 from openstef.logging.structlog_logger import StructlogLogger
 from openstef.settings import Settings
-
-
-class LoggerType(StrEnum):
-    # Alternatively, you can explicitly set the values:
-    STANDARD = "logging"
-    STRUCTLOG = "structlog"
 
 
 def get_logger(name: str, logger_type: str = Settings.logger_type):
---
+++ openstef/logging/logger_types.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from enum import StrEnum
+
+
+class LoggerType(StrEnum):
+    STANDARD = "logging"
+    STRUCTLOG = "structlog"
