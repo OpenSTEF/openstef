6d7dd9ac59862868723d9ea0a2f4f20a61382057
bartpleiter <a.t.pleiter@gmail.com>
2025-03-07 16:07:45 +0100

    fix: license.
    Signed-off-by: bartpleiter <a.t.pleiter@gmail.com>

--- openstef/logging/logger_factory.py
+++ openstef/logging/logger_factory.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <korte.termijn.prognoses@alliander.com> # noqa E501>
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from openstef.logging.logger_types import LoggerType
 from openstef.logging.standard_logger import StandardLogger
 from openstef.logging.structlog_logger import StructlogLogger
