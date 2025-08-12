f95d82ed6becdf8a15d68c75fc539488f5035fc5
Egor Dmitriev <egor.dmitriev@alliander.com>
2025-08-12 11:56:17 +0200

    feature(STEF-2298): [OpenSTEF 4.0] Setup project dev tools (#639)
    * feature(STEF-2297): Initial commit. Cleared OpenSTEF 3.0 code.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2297): Updated readme to exclude 3.0 specific parts.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2297): Created directory structure for the 4.0.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Configured Reuse tool.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Created reuse fix tool.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Added initial ruff config.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Configured ruff, poe, pytest and pyright.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Configured pyproject-fmt.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Added example scripts for ci testing. Added example integration test.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    * feature(STEF-2298): Added support for doctests.
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    ---------
    
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>

--- packages/openstef-beam/src/openstef_beam/__init__.py
+++ packages/openstef-beam/src/openstef_beam/__init__.py
@@ -1,3 +1,4 @@
 # SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
 #
 # SPDX-License-Identifier: MPL-2.0
+"""Backtesting, Evaluation, Analysis and Metrics (BEAM) library for OpenSTEF."""
---
+++ packages/openstef-models/tests/integration/test_config_integration.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+"""
+Integration test example for OpenSTEF Beam config utilities.
+
+This test is marked as an integration test and does not perform any actions.
+"""
+
+import pytest
+
+pytestmark = pytest.mark.integration
+
+
+def test_integration_example():
+    """Example integration test (does nothing)."""
