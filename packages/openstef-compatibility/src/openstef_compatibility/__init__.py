3488f5f7d0380cb95c757baa7142bcc6d08253d0
Egor Dmitriev <egor.dmitriev@alliander.com>
2025-08-20 11:27:58 +0200

    feature(#629): Added openstef compatibility package.
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>

---
+++ packages/openstef-compatibility/src/openstef_compatibility/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""OpenSTEF 3.0 compatibility layer for OpenSTEF 4.0 interfaces."""
