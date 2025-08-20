186570eeea1c6b58f56aecdb56c807fe0fd75078
Egor Dmitriev <egor.dmitriev@alliander.com>
2025-08-20 16:32:16 +0200

    feature(#628): Added openstef core package.
    Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>

---
+++ packages/openstef-core/src/openstef_core/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2017-2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""Core functionality for OpenSTEF, a framework for short-term energy forecasting."""
