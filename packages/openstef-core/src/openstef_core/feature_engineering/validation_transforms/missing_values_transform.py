08779a6dd88a0fe2cbc8e356ba23ce539ab636aa
Marnix van Lieshout <marnix.van.lieshout@alliander.com>
2025-08-25 15:31:25 +0200

    Add to pyproject
    Signed-off-by: Marnix van Lieshout <marnix.van.lieshout@alliander.com>

--- packages/openstef-core/src/openstef_core/feature_engineering/validation_transforms/missing_values_transform.py
+++ packages/openstef-core/src/openstef_core/feature_engineering/validation_transforms/missing_values_transform.py
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 from enum import Enum
-from typing_extensions import Union
 
 import numpy as np
-import pandas as pd
 from sklearn.impute import SimpleImputer
+
 from openstef_core.base_model import BaseConfig
 from openstef_core.datasets import TimeSeriesDataset
 from openstef_core.datasets.transforms import TimeSeriesTransform
+
 
 class ImputationStrategy(Enum):
     MEAN = "mean"
@@ -18,21 +18,22 @@ class ImputationStrategy(Enum):
     MOST_FREQUENT = "most_frequent"
     CONSTANT = "constant"
 
+
 class MissingValuesTransformConfig(BaseConfig):
-    """
-    Configuration class for MissingValuesCheck transform.
+    """Configuration class for MissingValuesCheck transform.
 
     Note: fill_value is used when imputation_strategy is CONSTANT.
     """
+
     missing_value: float = np.nan
     imputation_strategy: ImputationStrategy
-    fill_value: Union[int, float, str, None] = None
-    no_fill_future_values_features: list[str] = [] # TODO: Use this...
+    fill_value: int | float | str | None = None
+    no_fill_future_values_features: list[str] = []  # TODO: Use this...
+
 
 class MissingValuesTransform(TimeSeriesTransform):
-    """
-    Transform that checks for, imputes and drops missing values in time series data.
-    """
+    """Transform that checks for, imputes and drops missing values in time series data."""
+
     def __init__(self, config: MissingValuesTransformConfig):
         self.config = config
 
@@ -50,4 +51,3 @@ class MissingValuesTransform(TimeSeriesTransform):
     def transform(self, data: TimeSeriesDataset) -> TimeSeriesDataset:
         data.data = self.imputer_.transform(data.data)
         return data
-
