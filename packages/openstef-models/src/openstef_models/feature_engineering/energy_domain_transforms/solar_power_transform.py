042cb47106be1532c66110af5b79ecc59c56ae18
Marnix van Lieshout <marnix.van.lieshout@alliander.com>
2025-09-04 10:46:31 +0200

    Init commit
    Signed-off-by: Marnix van Lieshout <marnix.van.lieshout@alliander.com>

---
+++ packages/openstef-models/src/openstef_models/feature_engineering/energy_domain_transforms/solar_power_transform.py
@@ -0,0 +1,13 @@
+class SolarPowerTransform:
+    def __init__(self):
+        pass
+
+    def transform(self):
+        """
+        Transforms for solar power.
+
+        Args:
+
+        Returns:
+        """
+        return NotImplementedError
---
+++ packages/openstef-models/src/openstef_models/feature_engineering/energy_domain_transforms/wind_power_transform.py
@@ -0,0 +1,13 @@
+class WindPowerTransform:
+    def __init__(self):
+        pass
+
+    def transform(self):
+        """
+        Transforms for wind power.
+
+        Args:
+
+        Returns:
+        """
+        return NotImplementedError
