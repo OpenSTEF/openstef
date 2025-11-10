a0f3015a150012dfdd1d3d7e8d3a18ffc18e20e1
Marnix van Lieshout <marnix.van.lieshout@alliander.com>
2025-11-10 10:49:39 +0100

    Apply changes
    Signed-off-by: Marnix van Lieshout <marnix.van.lieshout@alliander.com>

--- packages/openstef-beam/src/openstef_beam/backtesting/backtest_event_generator.py
+++ packages/openstef-beam/src/openstef_beam/backtesting/backtest_event_generator.py
@@ -11,6 +11,7 @@ Acts as the temporal coordinator between data availability and model operations.
 
 from collections.abc import Iterator
 from datetime import datetime, time, timedelta
+from typing import cast
 
 import pandas as pd
 from pydantic import Field
@@ -129,7 +130,8 @@ class BacktestEventGenerator(BaseModel):
                 break
 
             training_start = max(
-                current_time - self.forecaster_config.training_context_length, min(self.index).to_pydatetime()
+                current_time - self.forecaster_config.training_context_length,
+                cast("pd.Series[pd.Timestamp]", self.index.min()).to_pydatetime()
             )
             training_end = current_time
             training_coverage = self._calculate_coverage(training_start, training_end)
