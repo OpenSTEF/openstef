ce2172a65c9a4ff469122165a340189ed5e040a0
Lars van Someren <lvsom1@gmail.com>
2025-11-17 21:17:07 +0100

    Squashed commit of the following:
    commit 37089b84bdea12d22506174ef1393c4fc346ca36
    Author: Egor Dmitriev <egor.dmitriev@alliander.com>
    Date:   Mon Nov 17 15:29:59 2025 +0100
    
        fix(#728): Fixed parallelism stability issues, and gblinear feature pipeline. (#752)
    
        * fix(STEF-2475): Added loky as default option for parallelism since fork causes instabilities for xgboost results.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix(STEF-2475): Added better support for flatliners and predicting when data is sparse.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix(STEF-2475): Feature handing improvements for gblinear. Like imputation, nan dropping, and checking if features are available.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix(#728): Added checks on metrics to gracefully handle empty data. Added flatline filtering during evalution.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix(#728): Updated xgboost to skip scaling on empty prediction.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix(STEF-2475): Added parallelism parameters.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        ---------
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    commit a85a3f709c9a54b85658578b5c2aefc001bdf803
    Author: Egor Dmitriev <egor.dmitriev@alliander.com>
    Date:   Fri Nov 14 14:31:34 2025 +0100
    
        fix(STEF-2475): Fixed rolling aggregate adder by adding forward filling and stating support for only one horizon. (#750)
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    commit 4f0c6648516bf184608d268020fdfa4107050c83
    Author: Egor Dmitriev <egor.dmitriev@alliander.com>
    Date:   Thu Nov 13 16:54:15 2025 +0100
    
        feature: Disabled data cutoff by default to be consistent with openstef 3.  And other minor improvements. (#748)
    
    commit 493126e9f16836d0da03d9c43e391537c5bea7ca
    Author: Egor Dmitriev <egor.dmitriev@alliander.com>
    Date:   Thu Nov 13 16:12:35 2025 +0100
    
        fix(STEF-2475) fix and refactor backtesting iction in context of backtestforecasting config for clarity. Added more colors. Fixed data split function to handle 0.0 splits. (#747)
    
        * fix: Fixed data collation during backtesting. Renamed horizon to prediction in context of backtestforecasting config for clarity. Added more colors. Fixed data split function to handle 0.0 splits.
    
        * fix: Formatting.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * fix: Formatting.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        ---------
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
    commit 6b1da449b7841f1b13a5fac1f16e48bbeb9b9692
    Author: Egor Dmitriev <egor.dmitriev@alliander.com>
    Date:   Thu Nov 13 16:05:32 2025 +0100
    
        feature: forecaster hyperparams and eval metrics (#746)
    
        * feature(#729) Removed to_state and from_state methods in favor of builtin python state saving functions.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * feature(#729): Fixed issue where generic transform pipeline could not be serialized.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * feature(#729): Added more state saving tests
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * feature(#729): Added more state saving tests
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * feature(#729): Added more state saving tests
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        * feature: standardized objective function. Added custom evaluation functions for forecasters.
    
        * fix: Formatting.
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>
    
        ---------
    
        Signed-off-by: Egor Dmitriev <egor.dmitriev@alliander.com>

---
+++ packages/openstef-models/src/openstef_models/utils/evaluation_functions.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""Utility functions for evaluation metrics in forecasting models."""
+
+from collections.abc import Callable
+from functools import partial
+from typing import Any, Literal
+
+import numpy as np
+
+from openstef_beam.metrics.metrics_probabilistic import mean_pinball_loss
+from openstef_core.types import Quantile
+
+type EvaluationFunctionType = Literal["mean_pinball_loss"]
+
+EVALUATION_MAP = {
+    "mean_pinball_loss": mean_pinball_loss,
+}
+
+
+def get_evaluation_function(
+    function_type: EvaluationFunctionType, quantiles: list[Quantile] | None = None, **kwargs: Any
+) -> Callable[[np.ndarray, np.ndarray], float]:
+    eval_metric = partial(EVALUATION_MAP[function_type], quantiles=quantiles, **kwargs)
+    eval_metric.__name__ = function_type  # pyright: ignore[reportAttributeAccessIssue]
+    return eval_metric
+
+
+__all__ = ["EVALUATION_MAP", "EvaluationFunctionType"]
