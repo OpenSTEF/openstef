7d953832a7189538a87c26075b0efb4a8876e966
Bart Pleiter <bart.pleiter@alliander.com>
2025-11-19 09:34:24 +0100

    fix(685): doctest skip for component splitter example.
    Signed-off-by: Bart Pleiter <bart.pleiter@alliander.com>

--- packages/openstef-models/src/openstef_models/models/component_splitting/dazls_component_splitter.py
+++ packages/openstef-models/src/openstef_models/models/component_splitting/dazls_component_splitter.py
@@ -86,7 +86,7 @@ class DazlsComponentSplitter(ComponentSplitter):
         ...     components=[EnergyComponentType.SOLAR, EnergyComponentType.WIND, EnergyComponentType.OTHER],
         ... )
         >>> splitter = DazlsComponentSplitter(config)
-        >>> # components = splitter.predict(time_series_data)
+        >>> components = splitter.predict(time_series_data) # doctest: +SKIP
     """
 
     _config: DazlsComponentSplitterConfig
