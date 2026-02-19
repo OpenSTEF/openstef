fe633498485f37d72aa1f908b411fb8b5eab71dc
Marnix van Lieshout <marnix.van.lieshout@alliander.com>
2026-02-19 13:07:43 +0100

    Delete empty folder
    Signed-off-by: Marnix van Lieshout <marnix.van.lieshout@alliander.com>

--- packages/openstef-meta/src/openstef_meta/models/forecasting/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2025 Contributors to the OpenSTEF project <short.term.energy.forecasts@alliander.com>
-#
-# SPDX-License-Identifier: MPL-2.0
-"""This module provides meta-forecasting models."""
