311cda54d391debf6742bc304181eb46ab19dbd6
Lars Schilders <123180911+lschilders@users.noreply.github.com>
2026-06-10 07:49:30 -0700

    chore: remove deprecated __setstate__ methods (#953)
    Signed-off-by: Lars Schilders <123180911+lschilders@users.noreply.github.com>
    Signed-off-by: Lars Schilders <lars.schilders@alliander.com>

--- packages/openstef-models/src/openstef_models/transforms/general/sample_weighter.py
+++ packages/openstef-models/src/openstef_models/transforms/general/sample_weighter.py
@@ -9,7 +9,7 @@ emphasizing high-value periods for improved model performance on peak loads.
 """
 
 import logging
-from typing import Any, Literal, cast, override
+from typing import Literal, override
 
 import numpy as np
 from pydantic import Field, PrivateAttr
@@ -195,30 +195,6 @@ class SampleWeighter(BaseConfig, TimeSeriesTransform):
     @override
     def features_added(self) -> list[str]:
         return [self.sample_weight_column]
-
-    @override
-    def __setstate__(self, state: dict[str, Any]) -> None:  # TODO(#799): delete after stable release
-        d = state["__dict__"]
-        fields_set = cast(set[str], state["__pydantic_fields_set__"])
-
-        # Migrate flat fields into nested SampleWeightConfig
-        if "config" not in d:
-            config_fields: dict[str, Any] = {}
-            for key in (
-                "method",
-                "weight_scale_percentile",
-                "weight_exponent",
-                "n_bins",
-                "dampening_exponent",
-                "weight_floor",
-            ):
-                if key in d:
-                    config_fields[key] = d.pop(key)
-                    fields_set.discard(key)
-            d["config"] = SampleWeightConfig(**config_fields)
-            fields_set.add("config")
-
-        return super().__setstate__(state)
 
 
 def exponential_sample_weight(
