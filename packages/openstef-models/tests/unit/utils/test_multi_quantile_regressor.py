874998d48643e5259d37a3d378867507fbbb8079
MentReeze <ment.reeze@alliander.com>
2026-07-20 14:06:48 +0200

    add early stopping to lgbm hyperparameters and enable to use in multi quantile regressor
    Signed-off-by: MentReeze <ment.reeze@alliander.com>

--- packages/openstef-models/tests/unit/utils/test_multi_quantile_regressor.py
+++ packages/openstef-models/tests/unit/utils/test_multi_quantile_regressor.py
@@ -117,3 +117,31 @@ def test_is_fitted_true_after_fit(dataset: tuple[pd.DataFrame, pd.Series], basel
 
     # Assert
     assert model.is_fitted
+
+
+def test_fit_forwards_eval_set_for_early_stopping_lgbm(dataset: tuple[pd.DataFrame, pd.Series]):
+    # Arrange
+    quantiles = [Quantile(0.1), Quantile(0.5), Quantile(0.9)]
+    X, y = dataset
+    y = y.to_numpy()
+    X_train, y_train = X.iloc[:80], y[:80]  # noqa: N806
+    X_val, y_val = X.iloc[80:], y[80:]  # noqa: N806
+    n_estimators = 100
+    model = MultiQuantileRegressor(
+        base_learner=LGBMRegressor,
+        quantile_param="alpha",
+        quantiles=quantiles,
+        hyperparams={
+            "objective": "quantile",
+            "n_estimators": n_estimators,
+            "early_stopping_rounds": 5,
+        },
+    )
+
+    # Act
+    model.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_val, y_val)])
+
+    # Assert: each per-quantile model stopped early instead of training all n_estimators.
+    for estimator in model.models:
+        assert isinstance(estimator, LGBMRegressor)
+        assert estimator.best_iteration_ < n_estimators
